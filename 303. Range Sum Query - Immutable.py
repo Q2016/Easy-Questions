da0ed45d9ba63310a0b4cdd89b4083c3ad53505e
Q2016 <k.sohraby@gmail.com>
2022-02-09 12:55:40 -0800

    Create 303. Range Sum Query - Immutable.py

---
+++ 303. Range Sum Query - Immutable.py
@@ -0,0 +1,24 @@
+Question:
+Given an integer array nums, handle multiple queries of the following type:
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive 
+(i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ 
+Example 1:
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Solution:
+class NumArray(object):
+    def __init__(self, nums):
+        self.dp = nums
+        for i in xrange(1, len(nums)):
+            self.dp[i] += self.dp[i-1]
+
+    def sumRange(self, i, j):
+        return self.dp[j] - (self.dp[i-1] if i > 0 else 0)
