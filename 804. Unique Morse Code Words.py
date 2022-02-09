13e558590723d0811b15a6dcaefa74d59c8f1403
Q2016 <k.sohraby@gmail.com>
2022-02-09 15:56:53 -0800

    Create 804. Unique Morse Code Words.py

---
+++ 804. Unique Morse Code Words.py
@@ -0,0 +1,38 @@
+Question:
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a 
+concatenation the transformation of a word. Return the number of different transformations among all words we have.
+
+Example 1:
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+
+
+Solution: Hash Set
+We can transform each word into it's Morse Code representation.
+After, we put all transformations into a set seen, and return the size of the set.
+
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        MORSE = [".-","-...","-.-.","-..",".","..-.","--.",
+                 "....","..",".---","-.-",".-..","--","-.",
+                 "---",".--.","--.-",".-.","...","-","..-",
+                 "...-",".--","-..-","-.--","--.."]
+
+        seen = {"".join(MORSE[ord(c) - ord('a')] for c in word)
+                for word in words}
+
+        return len(seen)
