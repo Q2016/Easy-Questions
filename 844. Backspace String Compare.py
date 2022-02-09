222dbd7590b4d3a8ec48e9301aab6f96e1672188
Q2016 <k.sohraby@gmail.com>
2022-02-09 09:41:19 -0800

    Create 844. Backspace String Compare.py

---
+++ 844. Backspace String Compare.py
@@ -0,0 +1,23 @@
+Question:
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+Note that after backspacing an empty text, the text will continue empty.
+
+Example 1:
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+  
+Solution:
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        
+        
+        def build(S):
+            ans = []
+            for c in S:
+                if c != '#':
+                    ans.append(c)
+                elif ans:
+                    ans.pop()
+            return "".join(ans)
+        return build(S) == build(T)  
