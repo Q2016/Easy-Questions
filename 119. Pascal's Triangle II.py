b1462587053e219a7414db40554d00b52d362c4b
Q2016 <k.sohraby@gmail.com>
2022-02-10 08:55:02 -0800

    Create 119. Pascal's Triangle II.py

---
+++ 119. Pascal's Triangle II.py
@@ -0,0 +1,17 @@
+Question:
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+Example 1:
+Input: rowIndex = 3
+Output: [1,3,3,1]
+
+
+Solution:
+class Solution(object):
+    def getRow(self, rowIndex):
+
+        row = [1]
+        for _ in range(rowIndex):
+            row = [x + y for x, y in zip([0]+row, row+[0])]
+        return row
