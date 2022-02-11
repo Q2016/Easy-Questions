86bf499f3ed1a17a4fde605fdc98cb71708fb307
Q2016 <k.sohraby@gmail.com>
2022-02-11 08:32:33 -0800

    Update 812. Largest Triangle Area.py

--- 812. Largest Triangle Area.py
+++ 812. Largest Triangle Area.py
@@ -1,21 +1,42 @@
-Explanaiton
-Burete force loop on all combinations of three points and calculate the area of these three points.
-If you google "three pointes triangle area formula", you can find the answer with the first result in second.
+Question:
+Given an array of points on the X-Y plane points where points[i] = [xi, yi], return the area of the largest triangle that can be 
+formed by any three different points. Answers within 10-5 of the actual answer will be accepted.
 
-Time complexity
-O(N^3) solution, but N <= 50, so it's fast enough.
-You may find convex hull first as @weidairpi replies. It help improve to O(M^3 + NlogN) in the best case where M is the number of points on the hull.
-But it make this easy problem complex and it stays same complexity in the worst case.
+Example 1:
+Input: points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+Output: 2.00000
+Explanation: The five points are shown in the above figure. The red triangle is the largest.
 
+Solution:
+Shoelace Method:
 
+class Solution:
+    def largestTriangleArea(self, p: List[List[int]]) -> float:
+        L, A = len(p), 0
+        for i in range(L-2):
+        	for j in range(i+1,L-1):
+        		for k in range(j+1,L):
+        			R = Area_Shoelace(p[i],p[j],p[k])
+        			A = max(A,R)
+        return A
 
+def Area_Shoelace(a,b,c):
+	return abs(a[0]*b[1]+b[0]*c[1]+c[0]*a[1]-(a[0]*c[1]+c[0]*b[1]+b[0]*a[1]))/2
+	
 
+Heron's Formula:
 
+class Solution:
+    def largestTriangleArea(self, p: List[List[int]]) -> float:
+        L, A = len(p), 0
+        for i in range(L-2):
+        	for j in range(i+1,L-1):
+        		for k in range(j+1,L):
+        			R = Area_Heron(p[i],p[j],p[k])
+        			A = max(A,R)
+        return A
 
-
-
-https://leetcode.com/problems/largest-triangle-area/discuss/122711/C%2B%2BJavaPython-Solution-with-Explanation-and-Prove
-def largestTriangleArea(self, p):
-  for i, j, k in itertools.combinations(p, 3))
-    return max(0.5 * abs(i[0] * j[1] + j[0] * k[1] + k[0] * i[1]- j[0] * i[1] - k[0] * j[1] - i[0] * k[1])
-            
+def Area_Heron(r,s,t):
+	a, b, c = math.hypot(r[0]-s[0],r[1]-s[1]), math.hypot(r[0]-t[0],r[1]-t[1]), math.hypot(s[0]-t[0],s[1]-t[1])
+	s = (a + b + c)/2
+	return (max(0,s*(s-a)*(s-b)*(s-c)))**.5
