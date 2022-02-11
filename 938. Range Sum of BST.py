02832cf740e836aa2378bd49951518ecd98c6bf6
Q2016 <k.sohraby@gmail.com>
2022-02-11 07:54:52 -0800

    Create 938. Range Sum of BST.py

---
+++ 938. Range Sum of BST.py
@@ -0,0 +1,44 @@
+Question:
+Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+Example 1:
+Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+Output: 32
+Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
+
+Solution:
+
+Method 1:
+
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        if not root:
+            return 0
+        elif root.val < L:
+            return self.rangeSumBST(root.right, L, R)
+        elif root.val > R:
+            return self.rangeSumBST(root.left, L, R)
+        return root.val + self.rangeSumBST(root.left, L, R) + self.rangeSumBST(root.right, L, R)
+
+The following are two more similar recursive codes.
+
+Method 2:
+
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        if not root:
+            return 0
+        return self.rangeSumBST(root.left, L, R) + \
+                self.rangeSumBST(root.right, L, R) + \
+                (root.val if L <= root.val <= R else 0)
+Method 3:
+
+    def rangeSumBST(self, root: TreeNode, L: int, R: int) -> int:
+        if not root:
+            return 0
+        sum = 0
+        if root.val > L:
+            sum += self.rangeSumBST(root.left, L, R)
+        if root.val < R:
+            sum += self.rangeSumBST(root.right, L, R)
+        if L <= root.val <= R:
+            sum += root.val     
+        return sum
