b7adba09cd7d16557570cc9345ed5e70627d6647
Q2016 <k.sohraby@gmail.com>
2022-02-11 07:41:28 -0800

    Create 27. Remove Element.py

---
+++ 27. Remove Element.py
@@ -0,0 +1,37 @@
+Question:
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the 
+array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. 
+It does not matter what you leave beyond the first k elements.
+Return k after placing the final result in the first k slots of nums.
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+Custom Judge:
+The judge will test your solution with the following code:
+int[] nums = [...]; // Input array
+int val = ...; // Value to remove
+int[] expectedNums = [...]; // The expected answer with correct length.
+                            // It is sorted with no values equaling val.
+
+int k = removeElement(nums, val); // Calls your implementation
+assert k == expectedNums.length;
+sort(nums, 0, k); // Sort the first k elements of nums
+for (int i = 0; i < actualLength; i++) {
+    assert nums[i] == expectedNums[i];
+}
+
+Solution: Two Pointers
+Since this question is asking us to remove all elements of the given value in-place, we have to handle it with O(1) extra space. 
+How to solve it? We can keep two pointers ii and j, where i is the slow-runner while j is the fast-runner.
+When nums[j]nums[j] equals to the given value, skip this element by incrementing jj. As long as nums[j] != val, we copy nums[j] to nums[i] 
+and increment both indexes at the same time. Repeat the process until j reaches the end of the array and the new length is i.
+
+def removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+}
