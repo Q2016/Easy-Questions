f1b1ef419c16916e429d592aa0986876bcb6022d
Q2016 <k.sohraby@gmail.com>
2022-02-11 17:27:46 -0800

    Create 485.Max Consecutive Ones.py

---
+++ 485.Max Consecutive Ones.py
@@ -0,0 +1,20 @@
+Question:
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+Example 1:
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+Solution:
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        cnt = 0
+        ans = 0
+        for num in nums:
+            if num == 1:
+                cnt += 1
+                ans = max(ans, cnt)
+            else:
+                cnt = 0
+        return ans
