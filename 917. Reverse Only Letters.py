076954cd37041db6acf491f0ed859dbcfd48a35d
Q2016 <k.sohraby@gmail.com>
2022-02-12 15:56:27 -0800

    Create 917. Reverse Only Letters.py

---
+++ 917. Reverse Only Letters.py
@@ -0,0 +1,23 @@
+Question:
+Given a string s, reverse the string according to the following rules: All the characters that are not English letters remain in the same position.
+All the English letters (lowercase or uppercase) should be reversed. Return s after reversing it.
+
+Example 1:
+Input: s = "ab-cd"
+Output: "dc-ba"
+
+Solution:Stack of Letters
+Collect the letters of S separately into a stack, so that popping the stack reverses the letters. (Alternatively, we could have collected 
+the letters into an array and reversed the array.) Then, when writing the characters of S, any time we need a letter, we use the 
+one we have prepared instead.
+
+class Solution(object):
+    def reverseOnlyLetters(self, S):
+        letters = [c for c in S if c.isalpha()]
+        ans = []
+        for c in S:
+            if c.isalpha():
+                ans.append(letters.pop())
+            else:
+                ans.append(c)
+        return "".join(ans)
