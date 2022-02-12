3361b0f717e1baaa8f5ee22b84b878fda9b32c00
Q2016 <k.sohraby@gmail.com>
2022-02-12 15:58:24 -0800

    Create 709. To Lower Case.py

---
+++ 709. To Lower Case.py
@@ -0,0 +1,17 @@
+Question:
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+Example 1:
+Input: s = "Hello"
+Output: "hello"
+
+Solution:
+class Solution:
+    def toLowerCase(self, str): 
+        return "".join(chr(ord(c) + 32) if 65 <= ord(c) <= 90 else c for c in str)
+class Solution:
+    def toLowerCase(self, str): 
+        return "".join(chr(ord(c) + 32) if "A" <= c <= "Z" else c for c in str)
+class Solution:
+    def toLowerCase(self, str): 
+        return str.lower()
