1f6b94a520e5ea6d7fdc9a0306598bc72ee9da7b
Q2016 <k.sohraby@gmail.com>
2022-02-12 17:35:43 -0800

    Create 700. Search in a Binary Search Tree.py

---
+++ 700. Search in a Binary Search Tree.py
@@ -0,0 +1,16 @@
+Question:
+You are given the root of a binary search tree (BST) and an integer val.
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+Example 1:
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+Solution:
+One way or another, DFS will continue till found target value val or will stop and return None from a leaf node
+
+class Solution:
+    def searchBST(self, root, val):
+        if root and val < root.val: return self.searchBST(root.left, val)
+        elif root and val > root.val: return self.searchBST(root.right, val)
+        return root
