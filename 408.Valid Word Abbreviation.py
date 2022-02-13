9a56f8175e7d5852e8525a43a176b8ea7656f501
Q2016 <k.sohraby@gmail.com>
2022-02-12 20:44:14 -0800

    Create 408.Valid Word Abbreviation.py

---
+++ 408.Valid Word Abbreviation.py
@@ -0,0 +1,31 @@
+Question:
+Given a non-empty string s and an abbreviation abbr, return whether the string matches with the given abbreviation.
+A string such as "word" contains only the following valid abbreviations:
+["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
+Notice that only the above abbreviations are valid abbreviations of the string "word". Any other string is not a valid abbreviation of "word".
+Example 1:
+Given s = "internationalization", abbr = "i12iz4n":
+Return true.
+
+Solution:
+
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+
+        w = 0
+        a = 0
+        while w < len(word) and a < len(abbr):
+            if abbr[a].isdigit() and abbr[a] != '0':
+                e = a
+                while e < len(abbr) and abbr[e].isdigit(): e += 1
+                num = int(abbr[a:e])
+                a = e
+                w += num
+            else:
+                if word[w] != abbr[a]:
+                    return False
+ 
+                w += 1
+                a += 1
+ 
+        return w == len(word) and a == len(abbr)
