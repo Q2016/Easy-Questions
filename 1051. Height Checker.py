3293fc3804dc9c8a3ef7a1fd7df7c45069dae2b5
Q2016 <k.sohraby@gmail.com>
2022-02-17 12:48:51 -0800

    Update 1051. Height Checker.py

--- 1051. Height Checker.py
+++ 1051. Height Checker.py
@@ -12,7 +12,7 @@ heights:  [1,1,4,2,1,3]
 expected: [1,1,1,2,3,4]
 Indices 2, 4, and 5 do not match.
 
-Solution:
+Solution: Sort
 
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
