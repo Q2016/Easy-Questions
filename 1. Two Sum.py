e890a539e57caa909883152e28d1fb3eca309219
Q2016 <k.sohraby@gmail.com>
2022-02-16 17:44:30 -0800

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -8,7 +8,7 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-Solution:
+Solution: Hashmap
 # https://www.youtube.com/watch?v=KLlXCFG5TnA
 # you can't solve it using two pointer or slidding window
 # my own solution with the hint of hashmap
