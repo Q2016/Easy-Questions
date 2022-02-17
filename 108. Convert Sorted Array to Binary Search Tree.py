c28a8cb9fc4ab7e981ff0cf41d5547049296a3f0
Q2016 <k.sohraby@gmail.com>
2022-02-17 14:19:44 -0800

    Update 108. Convert Sorted Array to Binary Search Tree.py

--- 108. Convert Sorted Array to Binary Search Tree.py
+++ 108. Convert Sorted Array to Binary Search Tree.py
@@ -7,7 +7,7 @@ Input: nums = [-10,-3,0,5,9]
 Output: [0,-3,9,-10,null,5]
 Explanation: [0,-10,5,null,-3,null,9] is also accepted:
 
-Solution:
+Solution: Recursive
 
 class Solution:
     def sortedArrayToBST(self, num):
