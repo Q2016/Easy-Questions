4276bc90578f72d4182f997bbdcaa1935f440ff5
Q2016 <k.sohraby@gmail.com>
2022-02-17 17:14:52 -0800

    Update 1287. Element Appearing More Than 25% In Sorted Array.py

--- 1287. Element Appearing More Than 25% In Sorted Array.py
+++ 1287. Element Appearing More Than 25% In Sorted Array.py
@@ -10,5 +10,5 @@ Solution:
   def findSpecialInteger(self, arr: List[int]) -> int:
       n = len(arr) // 4
       for i in range(len(arr)):
-      if arr[i] == arr[i + n]:
+          if arr[i] == arr[i + n]:
       return arr[i]
