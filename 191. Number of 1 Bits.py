ef4a0307ef8f294983d8b3f36fa1392a11db8270
Q2016 <k.sohraby@gmail.com>
2022-02-19 14:27:52 -0800

    Update 191. Number of 1 Bits.py

--- 191. Number of 1 Bits.py
+++ 191. Number of 1 Bits.py
@@ -1,33 +1,22 @@
 Question:
 Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
-Note:
-Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. 
-It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
-In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
- 
+
 Example 1:
 Input: n = 00000000000000000000000000001011
 Output: 3
 Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
 
+ 
 Solution:
 1.Built in solution with built-in function:
 
 def hammingWeight(self, n):
-    """
-    :type n: int
-    :rtype: int
-    """
     return bin(n).count('1')
-2.Using bit operation to cancel a 1 in each round
 
+2.Using bit operation to cancel a 1 in each round
 Think of a number in binary n = XXXXXX1000, n - 1 is XXXXXX0111. n & (n - 1) will be XXXXXX0000 which is just remove the last significant 1
 
 def hammingWeight(self, n):
-    """
-    :type n: int
-    :rtype: int
-    """
     c = 0
     while n:
         n &= n - 1
