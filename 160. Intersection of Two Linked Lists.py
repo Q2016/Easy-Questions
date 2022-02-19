c040e198d1d0c2306e84abacf019d0e4952f3f29
Q2016 <k.sohraby@gmail.com>
2022-02-19 11:26:11 -0800

    Update 160. Intersection of Two Linked Lists.py

--- 160. Intersection of Two Linked Lists.py
+++ 160. Intersection of Two Linked Lists.py
@@ -1,21 +1,16 @@
 Question:
 Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked 
 lists have no intersection at all, return null.
-For example, the following two linked lists begin to intersect at node c1:
-The test cases are generated such that there are no cycles anywhere in the entire linked structure.
-Note that the linked lists must retain their original structure after the function returns.
-Custom Judge:
-The inputs to the judge are given as follows (your program is not given these inputs):
-intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
-listA - The first linked list.
-listB - The second linked list.
-skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
-skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
-The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. 
-If you correctly return the intersected node, then your solution will be accepted.
 
 
 Solution:
+    
+# the idea is if you switch head, the possible difference between length would be countered. 
+# On the second traversal, they either hit or miss. 
+# if they meet, pa or pb would be the node we are looking for, 
+# if they didn't meet, they will hit the end at the same iteration, pa == pb == None, return either one of them is the same,None    
+    
+    
 class Solution:
     # @param two ListNodes
     # @return the intersected ListNode
@@ -34,7 +29,4 @@ class Solution:
 
         return pa # only 2 ways to get out of the loop, they meet or the both hit the end=None
 
-# the idea is if you switch head, the possible difference between length would be countered. 
-# On the second traversal, they either hit or miss. 
-# if they meet, pa or pb would be the node we are looking for, 
-# if they didn't meet, they will hit the end at the same iteration, pa == pb == None, return either one of them is the same,None
+
