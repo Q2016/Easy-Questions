efecd081ea70d6b706f856cab8443968278a1cd5
Q2016 <k.sohraby@gmail.com>
2022-02-19 18:30:10 -0800

    Update 232. Implement Queue using Stacks.py

--- 232. Implement Queue using Stacks.py
+++ 232. Implement Queue using Stacks.py
@@ -6,10 +6,7 @@ void push(int x) Pushes element x to the back of the queue.
 int pop() Removes the element from the front of the queue and returns it.
 int peek() Returns the element at the front of the queue.
 boolean empty() Returns true if the queue is empty, false otherwise.
-Notes:
-You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
-Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) 
-as long as you use only a stack's standard operations.
+
 
 
 Solution:
@@ -18,42 +15,23 @@ and add a "move()" method to simplify code: it moves all elements of the "inStac
 
 class Queue(object):
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
         self.inStack, self.outStack = [], []
 
     def push(self, x):
-        """
-        :type x: int
-        :rtype: nothing
-        """
         self.inStack.append(x)
 
     def pop(self):
-        """
-        :rtype: nothing
-        """
         self.move()
         self.outStack.pop()
 
     def peek(self):
-        """
-        :rtype: int
-        """
         self.move()
         return self.outStack[-1]
 
     def empty(self):
-        """
-        :rtype: bool
-        """
         return (not self.inStack) and (not self.outStack) 
         
     def move(self):
-        """
-        :rtype nothing
-        """
         if not self.outStack:
             while self.inStack:
                 self.outStack.append(self.inStack.pop())
