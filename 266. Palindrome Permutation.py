c72e164c98ef93ee30ede0574f10ed3a7d8d48cb
Q2016 <k.sohraby@gmail.com>
2022-02-20 10:59:52 -0800

    Update 266. Palindrome Permutation.py

--- 266. Palindrome Permutation.py
+++ 266. Palindrome Permutation.py
@@ -6,7 +6,7 @@ Hint:
 Consider the palindromes of odd vs even length. What difference do you notice? Count the frequency of each character. 
 If each character occurs even number of times, then it must be a palindrome. How about character which occurs odd number of times?
 
-Solution:
+Solution: Set
 class Solution(object):
     def canPermutePalindrome(self, s):
 
