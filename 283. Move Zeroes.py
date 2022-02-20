d4058ba9ac8ee12217817a962a6b8e54a49ba173
Q2016 <k.sohraby@gmail.com>
2022-02-20 12:20:30 -0800

    Update 283. Move Zeroes.py

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -6,28 +6,11 @@ Example 1:
 Input: nums = [0,1,0,3,12]
 Output: [1,3,12,0,0]
 
-Solution:
-# this solution is "not" good look at the O(1) solution 
-
-
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        
-        n=0
-        l=len(nums)
-        for i in range(l):
-            if nums[i]==0:
-                n+=1
-        
-        i=0
-
-        while i< l:
-            if nums[i]==0:
-
-                for j in range(i,l-1):
-                    nums[j]=nums[j+1]
-            else:
-                i+=1
-
-        for i in range(n):
-            nums[l-1-i]=0
+Solution: In-place
+    
+def moveZeroes(self, nums):
+    zero = 0  # records the position of "0"
+    for i in xrange(len(nums)):
+        if nums[i] != 0:
+            nums[i], nums[zero] = nums[zero], nums[i]
+            zero += 1
