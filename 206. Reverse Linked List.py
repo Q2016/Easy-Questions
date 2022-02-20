97b242d1b6759fb1860d4163d4761323799a927f
Q2016 <k.sohraby@gmail.com>
2022-02-19 17:38:28 -0800

    Update 206. Reverse Linked List.py

--- 206. Reverse Linked List.py
+++ 206. Reverse Linked List.py
@@ -4,22 +4,17 @@ Given the head of a singly linked list, reverse the list, and return the reverse
  
   
 Solution:
-# https://programmercave0.github.io/blog/2018/01/23/C++-Reverse-the-Linked-List-(Iterative-Method)-program
+Graphics 
+https://leetcode.com/problems/reverse-linked-list/discuss/1449712/Easy-C%2B%2BJavaPythonJavaScript-Explained%2BAnimated 
+ 
+     def reverseList(self, head):
+         prev = None
+         cur = head
 
+         while curr:
+             n = cur.next
+             cur.next = prev
+             prev = cur
+             cur = n
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        
-        prev=None
-        
-        while head:
-            curr=head
-            head=head.next
-            curr.next=prev
-            prev=curr    
+         return prev
