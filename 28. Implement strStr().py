9862949d36cf9e87d85bff5d992b08416ad44407
Q2016 <k.sohraby@gmail.com>
2022-02-20 12:18:06 -0800

    Update 28. Implement strStr().py

--- 28. Implement strStr().py
+++ 28. Implement strStr().py
@@ -10,13 +10,9 @@ Input: haystack = "hello", needle = "ll"
 Output: 2
 
 Solution:
-class Solution(object):
+
 def strStr(self, haystack, needle):
-    """
-    :type haystack: str
-    :type needle: str
-    :rtype: int
-    """
+
     for i in range(len(haystack) - len(needle)+1):
         if haystack[i:i+len(needle)] == needle:
             return i
