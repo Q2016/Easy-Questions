bbf86a579b333afdc5febbca060a7f2fa8706ea6
Q2016 <k.sohraby@gmail.com>
2022-02-20 11:21:18 -0800

    Update 27. Remove Element.py

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -5,33 +5,19 @@ array nums. More formally, if there are k elements after removing the duplicates
 It does not matter what you leave beyond the first k elements.
 Return k after placing the final result in the first k slots of nums.
 Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
-Custom Judge:
-The judge will test your solution with the following code:
-int[] nums = [...]; // Input array
-int val = ...; // Value to remove
-int[] expectedNums = [...]; // The expected answer with correct length.
-                            // It is sorted with no values equaling val.
 
-int k = removeElement(nums, val); // Calls your implementation
-assert k == expectedNums.length;
-sort(nums, 0, k); // Sort the first k elements of nums
-for (int i = 0; i < actualLength; i++) {
-    assert nums[i] == expectedNums[i];
-}
 
 Solution: Two Pointers
 Since this question is asking us to remove all elements of the given value in-place, we have to handle it with O(1) extra space. 
-How to solve it? We can keep two pointers ii and j, where i is the slow-runner while j is the fast-runner.
-When nums[j]nums[j] equals to the given value, skip this element by incrementing jj. As long as nums[j] != val, we copy nums[j] to nums[i] 
+How to solve it? We can keep two pointers i and j, where i is the slow-runner while j is the fast-runner.
+When nums[j] equals to the given value, skip this element by incrementing j. As long as nums[j] != val, we copy nums[j] to nums[i] 
 and increment both indexes at the same time. Repeat the process until j reaches the end of the array and the new length is i.
 
-def removeElement(int[] nums, int val) {
-    int i = 0;
-    for (int j = 0; j < nums.length; j++) {
-        if (nums[j] != val) {
-            nums[i] = nums[j];
-            i++;
-        }
-    }
-    return i;
-}
+    def removeElement(nums, val) :
+        i = 0
+        for j in range(len(nums)) :
+            if (nums[j] != val) :
+                nums[i] = nums[j]
+                i+=1
+        return i;
+
