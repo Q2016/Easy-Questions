cfdf97e819bccf1219f9bc5a1dd68b009abd1d06
Q2016 <k.sohraby@gmail.com>
2022-02-19 16:12:55 -0800

    Update 204. Count Primes.py

--- 204. Count Primes.py
+++ 204. Count Primes.py
@@ -8,15 +8,17 @@ Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 
 
 Solution:
-class Solution:
-# @param {integer} n
-# @return {integer}
-def countPrimes(self, n):
-    if n < 3:
-        return 0
-    primes = [True] * n
-    primes[0] = primes[1] = False
-    for i in range(2, int(n ** 0.5) + 1):
-        if primes[i]:
-            primes[i * i: n: i] = [False] * len(primes[i * i: n: i])
-    return sum(primes)
+Animation: https://leetcode.com/problems/count-primes/discuss/1200796/JS-Python-Java-C%2B%2B-or-Easy-Sieve-of-Eratosthenes-Solution-w-Explanation    
+    
+    
+    def countPrimes(self, n: int) -> int:
+
+        if n <= 2:
+            return 0
+        res = [True] * n
+        res[0] = res[1] = False
+        for i in range(2, n):
+            if res[i] == True:
+                for j in range(i, int((n-1)/i+1)):
+                    res[i*j] = False
+        return sum(res)
