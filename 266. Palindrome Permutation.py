9cbd30bd0678bf6d06c257ef172550f4fdb4b04a
Q2016 <k.sohraby@gmail.com>
2022-02-20 10:48:41 -0800

    Update 266. Palindrome Permutation.py

--- 266. Palindrome Permutation.py
+++ 266. Palindrome Permutation.py
@@ -11,11 +11,9 @@ class Solution(object):
     def canPermutePalindrome(self, s):
 
         oddChars = set()
-
         for c in s:
             if c in oddChars:
                 oddChars.remove(c)
             else:
                 oddChars.add(c)
-
         return len(oddChars) <= 1
