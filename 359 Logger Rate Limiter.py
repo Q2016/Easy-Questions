553044553c4ab046dec0635f7b87f3890b1624d9
Q2016 <k.sohraby@gmail.com>
2022-02-21 11:09:26 -0800

    Update 359 Logger Rate Limiter.py

--- 359 Logger Rate Limiter.py
+++ 359 Logger Rate Limiter.py
@@ -14,7 +14,10 @@ class Logger:
         self.dic = {}
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        """ Returns true if the message should be printed in the given timestamp, otherwise returns false. If this method returns false, the message will not be printed. The timestamp is in seconds granularity. """
+        """ Returns true if the message should be printed in the given timestamp, otherwise returns false. 
+        If this method returns false, the message will not be printed. The timestamp is in seconds granularity. 
+        """
+        
         if message not in self.dic or self.dic[message] + 10 <= timestamp:
             self.dic[message] = timestamp
             return True
