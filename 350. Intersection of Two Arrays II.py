92eb35a5db79a1bf49a24181571694c75c20871e
Q2016 <k.sohraby@gmail.com>
2022-02-21 11:01:55 -0800

    Update 350. Intersection of Two Arrays II.py

--- 350. Intersection of Two Arrays II.py
+++ 350. Intersection of Two Arrays II.py
@@ -6,15 +6,13 @@ Example 1:
 Input: nums1 = [1,2,2,1], nums2 = [2,2]
 Output: [2,2]
 
-Solution: Two pointers:
+Solution: Two pointers or Counter
 
-class Solution(object):
+
     def intersect(self, nums1, nums2):
-
         nums1, nums2 = sorted(nums1), sorted(nums2)
         pt1 = pt2 = 0
         res = []
-
         while True:
             try:
                 if nums1[pt1] > nums2[pt2]:
@@ -27,36 +25,30 @@ class Solution(object):
                     pt2 += 1
             except IndexError:
                 break
-
         return res
-use dictionary to count:
+    
+    
+or use dictionary to count:
 
-class Solution(object):
     def intersect(self, nums1, nums2):
-
         counts = {}
         res = []
-
         for num in nums1:
             counts[num] = counts.get(num, 0) + 1
-
         for num in nums2:
             if num in counts and counts[num] > 0:
                 res.append(num)
                 counts[num] -= 1
-
         return res
-use Counter to make it cleaner:
+    
 
-class Solution(object):
+and use Counter to make it cleaner:
+
     def intersect(self, nums1, nums2):
-
         counts = collections.Counter(nums1)
         res = []
-
         for num in nums2:
             if counts[num] > 0:
                 res += num,
                 counts[num] -= 1
-
         return res
