7b85c08593b745a6a4bba1560dbace08741bbc88
Q2016 <k.sohraby@gmail.com>
2022-02-20 18:48:16 -0800

    Update 290. Word Pattern.py

--- 290. Word Pattern.py
+++ 290. Word Pattern.py
@@ -7,7 +7,10 @@ Input: pattern = "abba", s = "dog cat cat dog"
 Output: true
   
 Solution: Hashmap
-At the first glance, the problem can be solved simply by using a hashmap w_to_p which maps words to letters from the pattern. But consider this example: w = ['dog', 'cat'] and p = 'aa'. In this case, the hashmap doesn't allow us to verify whether we can assign the letter a as a value to the key cat. This case can be handled by comparing length of the unique letters from the pattern and unique words from the string.
+At the first glance, the problem can be solved simply by using a hashmap w_to_p which maps words to letters from the pattern. 
+But consider this example: w = ['dog', 'cat'] and p = 'aa'. In this case, the hashmap doesn't allow us to verify whether we can 
+assign the letter a as a value to the key cat. This case can be handled by comparing length of the unique letters from the 
+pattern and unique words from the string.
 
 Space: O(n) - scan
 Time: O(n) - for the hashmap
