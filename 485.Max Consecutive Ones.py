1e38ae4515641b1f4d4533c45491527194e50684
Q2016 <k.sohraby@gmail.com>
2022-02-21 16:48:13 -0800

    Update 485.Max Consecutive Ones.py

--- 485.Max Consecutive Ones.py
+++ 485.Max Consecutive Ones.py
@@ -6,8 +6,10 @@ Input: nums = [1,1,0,1,1,1]
 Output: 3
 Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
 
-Solution:
-class Solution(object):
+    
+    
+Solution: One pass
+
     def findMaxConsecutiveOnes(self, nums):
         cnt = 0
         ans = 0
