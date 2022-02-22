f4cbb7f4802ac402142ed6680f1ad1036cea4706
Q2016 <k.sohraby@gmail.com>
2022-02-22 09:54:12 -0800

    Update 572. Subtree of Another Tree.py

--- 572. Subtree of Another Tree.py
+++ 572. Subtree of Another Tree.py
@@ -7,7 +7,7 @@ Example 1:
 Input: root = [3,4,5,1,2], subRoot = [4,1,2]
 Output: true
 
-Solution:
+Solution: DFS
 Naive approach, O(|s| * |t|)
 For each node of s, let's check if it's subtree equals t. We can do that in a straightforward way by an isMatch function: check if 
 s and t match at the values of their roots, plus their subtrees match. Then, in our main function, we want to check if s and t match, or 
@@ -17,8 +17,7 @@ def isMatch(self, s, t):
     if not(s and t):
         return s is t
     return (s.val == t.val and 
-            self.isMatch(s.left, t.left) and 
-            self.isMatch(s.right, t.right))
+            self.isMatch(s.left, t.left) and self.isMatch(s.right, t.right))
 
 def isSubtree(self, s, t): # main function
     if self.isMatch(s, t): return True
