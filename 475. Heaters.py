0cc87e7c19e4f10706506794c6b12af871558ceb
Q2016 <k.sohraby@gmail.com>
2022-02-21 16:25:09 -0800

    Update 475. Heaters.py

--- 475. Heaters.py
+++ 475. Heaters.py
@@ -1,19 +1,16 @@
 Qustion:
-Winter is coming! During the contest, your first job is to design a standard heater with a fixed warm radius to warm all the houses.
-Every house can be warmed, as long as the house is within the heater's warm radius range. 
-Given the positions of houses and heaters on a horizontal line, return the minimum radius standard of heaters so that those heaters could cover all houses.
-Notice that all the heaters follow your radius standard, and the warm radius will the same.
-
- 
+Winter is coming! During the contest, your first job is to design a standard heater with a fixed warm radius to warm all 
+the houses. Every house can be warmed, as long as the house is within the heater's warm radius range. 
+Given the positions of houses and heaters on a horizontal line, return the minimum radius standard of heaters so that those 
+heaters could cover all houses.
 
 Example 1:
-
 Input: houses = [1,2,3], heaters = [2]
 Output: 1
 Explanation: The only heater was placed in the position 2, and if we use the radius 1 standard, then all the houses can be warmed.
 
 
-Solution:
+Solution: BST
 class Solution:
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         
