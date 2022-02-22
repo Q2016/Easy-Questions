0fe2ddecb14e168eb1361180099f66ba27a0a89c
Q2016 <k.sohraby@gmail.com>
2022-02-21 21:20:31 -0800

    Update 501. Find Mode in Binary Search Tree.py

--- 501. Find Mode in Binary Search Tree.py
+++ 501. Find Mode in Binary Search Tree.py
@@ -1,13 +1,13 @@
 Question:
 Given the root of a binary search tree (BST) with duplicates, return all the mode(s) (i.e., the most frequently occurred element) in it.
 If the tree has more than one mode, return them in any order.
-Assume a BST is defined as follows:
-The left subtree of a node contains only nodes with keys less than or equal to the node's key.
-The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
-Both the left and right subtrees must also be binary search trees.
+
 
 Solution: Inorder traversal
-Inorder traversal of a BST will find the nodes in ascending order. So just compare the current node to the previous, and if they match, increase the current count of duplicate values by 1. If they don't match, reset the current count to 1. Compare the current count to the max count found so far. If they match, append the current value to the result list. If the current count of duplicates exceeds the max count, create a new result list with just the current value.
+Inorder traversal of a BST will find the nodes in ascending order. So just compare the current node to the previous, and if they match, 
+increase the current count of duplicate values by 1. If they don't match, reset the current count to 1. Compare the current count to the 
+max count found so far. If they match, append the current value to the result list. If the current count of duplicates exceeds the max count, 
+create a new result list with just the current value.
 
 class Solution(object):
     prev = None
