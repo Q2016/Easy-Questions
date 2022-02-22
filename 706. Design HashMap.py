aaec6d09b4a80d98d4111c55332647a8b3f0f842
Q2016 <k.sohraby@gmail.com>
2022-02-22 15:51:06 -0800

    Update 706. Design HashMap.py

--- 706. Design HashMap.py
+++ 706. Design HashMap.py
@@ -26,22 +26,11 @@ class ListNode:
         self.next = None
 
 class MyHashMap:
-
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.m = 1000;
         self.h = [None]*self.m
         
-
     def put(self, key, value):
-        """
-        value will always be non-negative.
-        :type key: int
-        :type value: int
-        :rtype: void
-        """
         index = key % self.m
         if self.h[index] == None:
             self.h[index] = ListNode(key, value)
@@ -55,13 +44,7 @@ class MyHashMap:
                 cur = cur.next
             cur.next = ListNode(key, value)
         
-
     def get(self, key):
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        :type key: int
-        :rtype: int
-        """
         index = key % self.m
         cur = self.h[index]
         while cur:
@@ -71,14 +54,7 @@ class MyHashMap:
                 cur = cur.next
         return -1
             
-        
-
     def remove(self, key):
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        :type key: int
-        :rtype: void
-        """
         index = key % self.m
         cur = prev = self.h[index]
         if not cur: return
@@ -92,11 +68,3 @@ class MyHashMap:
                     break
                 else:
                     cur, prev = cur.next, prev.next
-                
-
-
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
