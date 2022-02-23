c26b4b2798257570dee30c4be3f6cb9f48eacc7c
Q2016 <k.sohraby@gmail.com>
2022-02-23 08:00:08 -0800

    Update 849. Maximize Distance to Closest Person.py

--- 849. Maximize Distance to Closest Person.py
+++ 849. Maximize Distance to Closest Person.py
@@ -13,20 +13,43 @@ If Alex sits in any other open seat, the closest person has distance 1.
 Thus, the maximum distance to the closest person is 2.
 
 Solution:
-class Solution:
+Here we have 4 main cases that need to cover:
+[1,0,0,0,1,0] -> 2 -> between ones
+[0,0,0,0,0,1] -> 5 -> one in end
+[1,0,0,0,0,0] -> 5 -> one in beggining
+[0,0,1,0,0,0] -> 3 -> one in middle
+Intuition:
+Let's have 2 pointers(prev and cur) for keep track ones:
+Always move current pointer to right:
+[1,0,0,0,1,0]
+ p
+ c
+Till meet next one:
+[1,0,0,0,1,0]
+ p
+         c
+Then need to check if we're between 2 ones (seats[prev] == 1):
+if yes, then maximize the result with (c - p) // 2      -> cover case #1
+if no, then maximize the result with (c - p)            -> cover case #2
+finally move prev to cur
+But cases 3 and 4 are not covered :(
+if seats[prev] points to one:
+[0,0,1,0,0,0]
+     p
+             c
+So, let's have a check in the end, like:
+if seats[prev] == 1 then let's maximize the result with len(seats) - 1 - prev -> cover cases #3 and #4
+Code:
+
     def maxDistToClosest(self, seats: List[int]) -> int:
-        
-        
-        ocu=[]
-        for i, n in enumerate(seats):
-            if n==1:
-                ocu.append(i)
-        
-        tmp=0
-        dis=0
-        for i in ocu:
-            tmp=i-tmp
-            dis=max(dis,tmp)
-            tmp=i
-            
-        return dis//2
+        prev, max_len = 0, 0
+        for cur, seat in enumerate(seats):
+            if seat:
+                if seats[prev]:
+                    max_len = max(max_len, (cur - prev) // 2)
+                else:
+                    max_len = max(max_len, (cur - prev))
+                prev = cur 
+        if seats[prev]: 
+            max_len = max(max_len, len(seats) - 1 - prev)     
+        return max_len
