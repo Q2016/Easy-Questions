f596646121afbe0d235701617607cd8eca86e061
Q2016 <k.sohraby@gmail.com>
2022-02-22 16:11:59 -0800

    Update 717. 1-bit and 2-bit Characters.py

--- 717. 1-bit and 2-bit Characters.py
+++ 717. 1-bit and 2-bit Characters.py
@@ -1,24 +1,35 @@
 Question:
-We have two special characters:
-The first character can be represented by one bit 0.
-The second character can be represented by two bits (10 or 11).
-Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
+We use three bit-representations to represent three characters : 0, 10, 11 (the corresponding characters don't matter)
+So bits representation 1, 01 are not valid. Given an array of bits ending with 0, find out if it's only valid to translate the last 0 into 
+the 0 bit representation (means it's not valid when the last 0 is translated into the 10 bits representation)
 
 Example 1:
 Input: bits = [1,0,0]
 Output: true
-Explanation: The only way to decode it is two-bit character and one-bit character.
+Explanation: The only way to decode it, is two-bit character and one-bit character.
 So the last character is one-bit character.
 
-Solution:
 
-    if not bits: return False
-    n = len(bits)
-    
-    index = 0
-    while index < n:
-        if index == n-1 : return True
-        if bits[index] == 1: 
-            index += 2              
-        else: index += 1
-    return False
+Solution:
+# (Time, Space) Complexity : O(n), O(1)
+
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        # Important Rules:
+        # 1. If bit n is 0, bit n+1 must be a new char
+        # 2. If bits end with 1, last bit must be a two bit char
+        #    However, this case had been rejected by question
+        # 3. If 1s in row and end with 0, 
+        #    we can use count or 1s to check last char
+        #    If count is even, last char is "0"
+        #    If count is odd,  last char is "10"
+        # Strategy:
+        # 1. We don't care last element, since it must be 0.
+        # 2. We check from reversed, and count 1s in a row
+        # 3. Once 0 occur or list end, We stop counting
+        # 4. We use count to determin result
+        # 5. Since we will mod count by 2, we simplify it to bool
+        ret = True
+        for bit in bits[-2::-1]:
+            if bit: ret = not ret
+            else: break
+        return ret
