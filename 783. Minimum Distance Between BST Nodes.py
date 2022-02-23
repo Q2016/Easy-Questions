a052541ee8497d49b81a595fb9870b5cac66598f
Q2016 <k.sohraby@gmail.com>
2022-02-22 17:14:02 -0800

    Update 783. Minimum Distance Between BST Nodes.py

--- 783. Minimum Distance Between BST Nodes.py
+++ 783. Minimum Distance Between BST Nodes.py
@@ -1,7 +1,7 @@
 Question:
 Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different nodes in the tree.
 
-Solution:
+Solution: Inorder traversal~sorted array
 The idea is to use an in-order traversal and find the difference between the current node and the node previously checked.
 Think of this question like a variation on "what is the smallest distance between any two values in a sorted array?" With a 
 sorted array, all you do is scan through the entire array looking for the absolute minimum difference between two adjacent values 
@@ -16,9 +16,8 @@ To scan the BST like a sorted array, just go through IN-ORDER traversal and keep
             return
         
         self.minDiffInBST(root.left)
-		# evaluate and set the current node as the node previously evaluated
+	# evaluate and set the current node as the node previously evaluated
         self.res = min(self.res, root.val - self.pre)
         self.pre = root.val
-		
         self.minDiffInBST(root.right)
         return self.res
