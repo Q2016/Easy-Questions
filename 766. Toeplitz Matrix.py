b83518d660f65a1495128be299fb7b5f43497f80
Q2016 <k.sohraby@gmail.com>
2022-02-22 17:06:22 -0800

    Update 766. Toeplitz Matrix.py

--- 766. Toeplitz Matrix.py
+++ 766. Toeplitz Matrix.py
@@ -3,26 +3,22 @@ Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return 
 A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
 
 Example 1:
-Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Input: matrix = [[1,2,3,4],
+                 [5,1,2,3],
+                 [9,5,1,2]]
 Output: true
 Explanation:
-In the above grid, the diagonals are:
-"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
-In each diagonal all elements are the same, so the answer is True.
+In the above grid, the diagonals are: "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]". In each diagonal all elements are the same, so the answer is True.
 
 Solution:
-class Solution:
+
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        
         n=len(matrix[0])
         m=len(matrix)
-        
         d = collections.defaultdict(list)
-        
         for i in range(m):
             for j in range(n):
-                d[i-j].append(matrix[i][j])
-                
+                d[i-j].append(matrix[i][j]) 
         for _, t in d.items():
             if len(set(t))!=1:
                 return False
