810cbf5a66cfff0d193e778147404109aab6847f
Q2016 <k.sohraby@gmail.com>
2022-02-22 16:46:51 -0800

    Update 747. Largest Number At Least Twice of Others.py

--- 747. Largest Number At Least Twice of Others.py
+++ 747. Largest Number At Least Twice of Others.py
@@ -5,15 +5,13 @@ twice as much as every other number in the array. If it is, return the index of 
 Example 1:
 Input: nums = [3,6,1,0]
 Output: 1
-Explanation: 6 is the largest integer.
-For every other number in the array x, 6 is at least twice as big as x.
-The index of value 6 is 1, so we return 1.
+Explanation: 6 is the largest integer. For every other number in the array x, 6 is at least twice as big as x. The index of value 6 is 1, so we return 1.
 
 
 Solution:
 Just iterate through the array and note the highest and second highest numbers. Might as well take note of the index at the same time.
-
-One slightly clever idea was to shuffle the highest to the second-highest whenever a new highest was found. That was a way to handle the case where there are two (or more) value tied for highest.
+One slightly clever idea was to shuffle the highest to the second-highest whenever a new highest was found. That was a way to handle the 
+case where there are two (or more) value tied for highest.
 
 class Solution:
     def dominantIndex(self, nums):
