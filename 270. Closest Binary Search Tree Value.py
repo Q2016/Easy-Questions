249612fd0f40654b60fa7a80ac8fdc7d22a9de50
Q2016 <k.sohraby@gmail.com>
2022-02-25 11:57:51 -0800

    Update 270. Closest Binary Search Tree Value.py

--- 270. Closest Binary Search Tree Value.py
+++ 270. Closest Binary Search Tree Value.py
@@ -1,6 +1,17 @@
 Question:
 Given the root of a binary search tree and a target value, return the value in the BST that is closest to the target.
 
+Example:
+Input: root = [4,2,5,1,3], target = 3.714286
+    4
+   / \
+  2   5
+ / \
+1   3
+Output: 4
+
+
+
 Solution:
 class Solution(object):
     def closestValue(self, root, target):
