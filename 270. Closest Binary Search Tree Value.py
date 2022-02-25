0769df17ce7ba3f4ccb22e744bff2936ef088fa4
Q2016 <k.sohraby@gmail.com>
2022-02-25 12:27:59 -0800

    Update 270. Closest Binary Search Tree Value.py

--- 270. Closest Binary Search Tree Value.py
+++ 270. Closest Binary Search Tree Value.py
@@ -12,13 +12,12 @@ Output: 4
 
 
 
-Solution:
-class Solution(object):
+Solution: !
+    
     def closestValue(self, root, target):
 
         if root is None:
             return float('inf')
-
         if root.val == target:
             return root.val
         elif root.val < target:
