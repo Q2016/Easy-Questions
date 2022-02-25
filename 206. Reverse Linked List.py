5ec23c55aba0386abde7ec7c2dae3a9d33f9c098
Q2016 <k.sohraby@gmail.com>
2022-02-24 16:36:04 -0800

    Update 206. Reverse Linked List.py

--- 206. Reverse Linked List.py
+++ 206. Reverse Linked List.py
@@ -7,6 +7,15 @@ Solution:
 Graphics 
 https://leetcode.com/problems/reverse-linked-list/discuss/1449712/Easy-C%2B%2BJavaPythonJavaScript-Explained%2BAnimated 
  
+The idea is very simple, we maintain 3 pointers, current, next and previous, abbreviated as cur, n and prev respectively. All the events occur in a chain.
+Preassign: assign prev=NULL, cur=head .
+Below are the steps:
+First we initialize n to be the node after cur. i.e(n=cur->next).
+Then we make cur->next point to prev (next node pointer).
+Then we make prev now point to (one node ahead) the cur node.
+At last we move cur also one node ahead to n.
+Follow the same steps untill the head points to NULL i.e there is no node to reverse. The prev pointer will be the last non null node and hence the answer. 
+
      def reverseList(self, head):
          prev = None
          cur = head
