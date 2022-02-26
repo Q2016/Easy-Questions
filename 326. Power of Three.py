96edb8dbec2e88a5811cf472cb096105e1ea6440
Q2016 <k.sohraby@gmail.com>
2022-02-25 21:27:14 -0800

    Update 326. Power of Three.py

--- 326. Power of Three.py
+++ 326. Power of Three.py
@@ -2,6 +2,8 @@ Question:
 Given an integer n, return true if it is a power of three. Otherwise, return false.
 An integer n is a power of three, if there exists an integer x such that n == 3x.
 
+
+
 Solution:
 One simple way of finding out if a number n is a power of a number b is to keep dividing n by b as long as the 
 remainder is 0. This is because we can write
