fcde0a0eb19aa619e60dd380d085f733b6a5d2f8
Q2016 <k.sohraby@gmail.com>
2022-02-26 09:00:48 -0800

    Update 401. Binary Watch.py

--- 401. Binary Watch.py
+++ 401. Binary Watch.py
@@ -10,7 +10,8 @@ Input: turnedOn = 1
 Output: ["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]
 
  
-Solution:
+ 
+Solution: Bin+string.count('x')
  
 def readBinaryWatch(self, num):
     return ['%d:%02d' % (h, m) for h in range(12) for m in range(60) if (bin(h) + bin(m)).count('1') == num]
