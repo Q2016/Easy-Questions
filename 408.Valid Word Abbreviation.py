88941522d66ca8162145c2556c4861e66d63205c
Q2016 <k.sohraby@gmail.com>
2022-02-26 10:28:22 -0800

    Update 408.Valid Word Abbreviation.py

--- 408.Valid Word Abbreviation.py
+++ 408.Valid Word Abbreviation.py
@@ -3,14 +3,14 @@ Given a non-empty string s and an abbreviation abbr, return whether the string m
 A string such as "word" contains only the following valid abbreviations:
 ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
 
-
 Example 1:
 Given s = "internationalization", abbr = "i12iz4n":
 Return true.
 
 
-Solution:
 
+Solution: Two loops
+It's one of the question that require leetcode membership
 
     def validWordAbbreviation(self, word, abbr):
         w = 0
