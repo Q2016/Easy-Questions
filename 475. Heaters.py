bbae3785a77af53b7b2aefdcd34fd74b1cc18e41
Q2016 <k.sohraby@gmail.com>
2022-02-26 18:01:18 -0800

    Update 475. Heaters.py

--- 475. Heaters.py
+++ 475. Heaters.py
@@ -1,31 +1,27 @@
 Qustion:
-Winter is coming! During the contest, your first job is to design a standard heater with a fixed warm radius to warm all 
-the houses. Every house can be warmed, as long as the house is within the heater's warm radius range. 
-Given the positions of houses and heaters on a horizontal line, return the minimum radius standard of heaters so that those 
-heaters could cover all houses.
+Design a standard heater with a fixed warm radius to warm all the houses. Every house can be warmed, as long as the house is within the 
+heater's warm radius range. Given the positions of houses and heaters on a horizontal line, return the minimum radius standard of heaters 
+so that those heaters could cover all houses.
 
 Example 1:
-Input: houses = [1,2,3], heaters = [2]
+Input: houses = [1,2,3,4], heaters = [1,4]
 Output: 1
-Explanation: The only heater was placed in the position 2, and if we use the radius 1 standard, then all the houses can be warmed.
+Explanation: The two heater was placed in the position 1 and 4. We need to use radius 1 standard, then all the houses can be warmed.
 
 
 Solution: BST
-class Solution:
+
     def findRadius(self, houses: List[int], heaters: List[int]) -> int:
         
         def binary_search_recursive(array, element, start, end):
             if start > end:
                 return -1
-
             mid = (start + end) // 2
             if element == array[mid]:
                 return mid
-
             if element < array[mid]:
                 return binary_search_recursive(array, element, start, mid-1)
             else:
                 return binary_search_recursive(array, element, mid+1, end)
-       
         for h in houses:
             binary_search_recursive(heaters, h, heaters[0], heaters[-1])
