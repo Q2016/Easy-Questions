2bf19bce13db38d996fcadc74f1f7b0334ccc86e
Q2016 <k.sohraby@gmail.com>
2022-02-27 05:57:27 -0800

    Update 521. Longest Uncommon Subsequence I.py

--- 521. Longest Uncommon Subsequence I.py
+++ 521. Longest Uncommon Subsequence I.py
@@ -18,7 +18,7 @@ These three cases are possible with string a and b:
 a=b. If both the strings are identical, it is obvious that no subsequence will be uncommon. Hence, return -1.
 length(a)=length(b) and a != b. Example: abcabc and abdabd. In this case we can consider any string i.e. abcabc or abdabd as a 
 required subsequence, as out of these two strings one string will never be a subsequence of other string. Hence, return length(a) or 
-length(b)length(b). length(a)!= length(b) Example abcdabcd and abcabc. In this case we can consider bigger string as a required subsequence 
+length(b). length(a)!= length(b) Example abcdabcd and abcabc. In this case we can consider bigger string as a required subsequence 
 because bigger string can't be a subsequence of smaller string. Hence, return max(length(a),length(b)).
 
 public class Solution {
