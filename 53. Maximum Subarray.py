d5f11e86ea7ac13365fc2f4814517c13d3b34d61
Q2016 <k.sohraby@gmail.com>
2022-02-27 06:29:14 -0800

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -8,9 +8,15 @@ Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 
 
-Solution:
-
+    
+Solution: Kadane's algorithm
 # https://www.youtube.com/watch?v=5WZl3MMT0Eg
+
+The thought follows a simple rule:
+If the sum of a subarray is positive, it has possible to make the next value bigger, so we keep do it until it turn to negative.
+If the sum is negative, it has no use to the next element, so we break.
+it is a game of sum, not the elements.
+
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
@@ -18,7 +24,7 @@ class Solution:
         curSum=0
         
         for n in nums:
-            if curSum<0:
+            if curSum<0: # it's assuming the max contiguous subarray can be negative!
                 curSum=0
             curSum+=n
             maxSub=max(maxSub, curSum)
