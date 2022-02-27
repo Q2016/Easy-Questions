1a203a7a6e92887337fa4a847171ae5993bee1ef
Q2016 <k.sohraby@gmail.com>
2022-02-27 09:20:10 -0800

    Update 665. Non-decreasing Array.py

--- 665. Non-decreasing Array.py
+++ 665. Non-decreasing Array.py
@@ -8,7 +8,7 @@ Output: true
 Explanation: You could modify the first 4 to 1 to get a non-decreasing array.
 
 
-Solution:
+Solution: Greedy
 This problem is like a greedy problem. When you find nums[i-1] > nums[i] for some i, you will prefer to change nums[i-1]'s value, 
 since a larger nums[i] will give you more risks that you get inversion errors after position i. But, if you also find nums[i-2] > nums[i], 
 then you have to change nums[i]'s value instead, or else you need to change both of nums[i-2]'s and nums[i-1]'s values.
