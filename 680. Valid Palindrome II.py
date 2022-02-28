5bba533f186770d377cbbefad7073b7b244bb583
Q2016 <k.sohraby@gmail.com>
2022-02-27 21:53:20 -0800

    Update 680. Valid Palindrome II.py

--- 680. Valid Palindrome II.py
+++ 680. Valid Palindrome II.py
@@ -5,24 +5,28 @@ Example 1:
 Input: s = "aba"
 Output: true
 
-Solution:
-We can use the standard two-pointer approach that starts at the left and right of the string and move inwards. 
-Whenever there is a mismatch, we can either exclude the character at the left or the right pointer. We then take the two 
-remaining substrings and compare against its reversed and see if either one is a palindrome.
-
-💯 Check out https://techinterviewhandbook.org for more tips and tricks to ace your technical interview 💯
-
-class Solution(object):
+    
+    
+Solution: Recursive
+For figure: https://leetcode.com/problems/valid-palindrome-ii/discuss/1452155/Python-Visual-Explanation-Picture-is-worth-a-thousand-words    
+    
     def validPalindrome(self, s):
-
-        left, right = 0, len(s) - 1
-        while left < right:
-            if s[left] != s[right]:
-                one, two = s[left:right], s[left + 1:right + 1]
-                return one == one[::-1] or two == two[::-1]
-            left, right = left + 1, right - 1
-        return True  
-      
-      
-# Time: O(n)
-# Space: O(n)      
+        i, j = 0, len(s) - 1
+        
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return self.validPalindromeUtil(s, i + 1, j) or self.validPalindromeUtil(s, i, j - 1)
+        return True
+    
+    def validPalindromeUtil(self, s, i, j):
+        while i < j:
+            if s[i] == s[j]:
+                i += 1
+                j -= 1
+            else:
+                return False
+        
+        return True     
