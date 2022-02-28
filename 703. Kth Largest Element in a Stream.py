0251258cd98d959790038ac701a94602021b7e3f
Q2016 <k.sohraby@gmail.com>
2022-02-28 09:51:58 -0800

    Update 703. Kth Largest Element in a Stream.py

--- 703. Kth Largest Element in a Stream.py
+++ 703. Kth Largest Element in a Stream.py
@@ -21,21 +21,18 @@ kthLargest.add(4);   // return 8
 
 
 Solution:
-So now that we know what a heap does, how does it help solve this problem? Let's say we have some stream of numbers, 
-nums = [6, 2, 3, 1, 7], and k = 3. Because the input is small, we can clearly see the kth smallest element is 3. Although, 
-earlier we said that a heap can only find an element in O(1) time if it's a minimum or maximum (depending on choice of implementation). 
+How does it help solve this problem? a heap can only find an element in O(1) time if it's a minimum or maximum (depending on choice of implementation). 
 Well, a heap is also capable of removing the smallest element quickly, so what if we just keep removing the smallest element from nums 
-until nums.length == k? In this case, we would have nums = [3, 6, 7], and a heap can now give us our answer in O(1) time.
-That's the key to solving this problem - use a min-heap (min means that the heap will remove/find the smallest element, a max heap is 
-the same thing but for the largest element) and keep the heap at size k. That way, the smallest element in the heap (the one we can access in O(1)) 
-will always be the kth largest element. This way, when adding a number to the heap with add(), we can do it very quickly in log(n) time. 
-If our heap exceeds size k, then we can also remove it very quickly. In the end, the smallest element in the heap will be the answer.
-class KthLargest:
+until nums.length == k? a heap can now give us our answer in O(1) time. That's the key to solving this problem - use a min-heap (min means that the heap 
+will remove/find the smallest element, a max heap is the same thing but for the largest element) and keep the heap at size k. That way, the smallest 
+element in the heap (the one we can access in O(1)) will always be the kth largest element. This way, when adding a number to the heap with add(), we 
+can do it very quickly in log(n) time. If our heap exceeds size k, then we can also remove it very quickly. In the end, the smallest element in the 
+heap will be the answer.
+
     def __init__(self, k: int, nums: List[int]):
         self.k = k
         self.heap = nums
         heapq.heapify(self.heap)
-        
         while len(self.heap) > k:
             heapq.heappop(self.heap)
 
