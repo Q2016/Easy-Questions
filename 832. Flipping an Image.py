13d6aafc29efb7f450b731f165cf4bf555c6ee56
Q2016 <k.sohraby@gmail.com>
2022-02-28 14:55:55 -0800

    Update 832. Flipping an Image.py

--- 832. Flipping an Image.py
+++ 832. Flipping an Image.py
@@ -6,13 +6,21 @@ To invert an image means that each 0 is replaced by 1, and each 1 is replaced by
 For example, inverting [0,1,1] results in [1,0,0].
 
 Example 1:
-Input: image = [[1,1,0],[1,0,1],[0,0,0]]
-Output: [[1,0,0],[0,1,0],[1,1,1]]
-Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
-Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+Input: image = [[1,1,0],
+                [1,0,1],
+                [0,0,0]]
+Output: [[1,0,0],
+         [0,1,0],
+         [1,1,1]]
+Explanation: First reverse each row: [[0,1,1],
+                                      [1,0,1],
+                                      [0,0,0]].
+Then, invert the image: [[1,0,0],
+                         [0,1,0],
+                         [1,1,1]]
 
     
-Solution:
+Solution: Inverse of binary matrix is 1-i per each element i
 We can do this in place. In each row, the ith value from the left is equal to the inverse of the ith value from the right.
 We use (C+1) / 2 (with floor division) to iterate over all indexes i in the first half of the row, including the center.
 
