261ee62070654f2d288385f3425a93596c48bf9b
Q2016 <k.sohraby@gmail.com>
2022-02-28 11:51:02 -0800

    Update 733. Flood Fill.py

--- 733. Flood Fill.py
+++ 733. Flood Fill.py
@@ -1,9 +1,7 @@
 Question:
-An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
 You are also given three integers sr, sc, and newColor. You should perform a flood fill on the image starting from the pixel image[sr][sc].
-To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color 
-as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color 
-of all of the aforementioned pixels with newColor. Return the modified image after performing the flood fill.
+To perform a flood fill, start from image[sr][sc] and replace with newColor any pixels connected 4-directionally. 
+Return the modified image after performing the flood fill.
 
 Example 1:
 Input: image = [[1,1,1],  sr = 1, sc = 1, newColor = 2
@@ -17,13 +15,14 @@ same color as the starting pixel (i.e., the blue pixels) are colored with the ne
 Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
 
 
+
 Solution: Depth-First Search
 We perform the algorithm explained in the problem description: paint the starting pixels, plus adjacent pixels of the same color, and so on.
 Say color is the color of the starting pixel. Let's floodfill the starting pixel: we change the color of that pixel to the new color, 
 then check the 4 neighboring pixels to make sure they are valid pixels of the same color, and of the valid ones, we floodfill those, and so on.
 We can use a function dfs to perform a floodfill on a target pixel.
 
-class Solution(object):
+
     def floodFill(self, image, sr, sc, newColor):
         R, C = len(image), len(image[0])
         color = image[sr][sc]
