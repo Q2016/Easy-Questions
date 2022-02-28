89aa6d9e6c9a1c109f763def905d582b26c190fc
Q2016 <k.sohraby@gmail.com>
2022-02-28 15:39:49 -0800

    Update 849. Maximize Distance to Closest Person.py

--- 849. Maximize Distance to Closest Person.py
+++ 849. Maximize Distance to Closest Person.py
@@ -1,8 +1,7 @@
 Question:
 You are given an array representing a row of seats where seats[i] = 1 represents a person sitting in the ith seat, and seats[i] = 0 
-represents that the ith seat is empty (0-indexed). There is at least one empty seat, and at least one person sitting.
-Alex wants to sit in the seat such that the distance between him and the closest person to him is maximized. 
-Return that maximum distance to the closest person.
+represents that the ith seat is empty. There is at least one empty seat, and at least one person sitting. Alex wants to sit in the seat such that 
+the distance between him and the closest person to him is maximized. Return that maximum distance to the closest person.
 
 Example 1:
 Input: seats = [1,0,0,0,1,0,1]
@@ -11,6 +10,9 @@ Explanation:
 If Alex sits in the second open seat (i.e. seats[2]), then the closest person has distance 2.
 If Alex sits in any other open seat, the closest person has distance 1.
 Thus, the maximum distance to the closest person is 2.
+
+
+
 
 Solution:
 Here we have 4 main cases that need to cover:
