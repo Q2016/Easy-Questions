b49d8f54a59f1154fa5956c14c3a30b3b6aa5743
Q2016 <k.sohraby@gmail.com>
2022-02-28 09:12:27 -0800

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -6,20 +6,18 @@ Example 1:
 Input: x = 123
 Output: 321
 
+    
+    
 Solution:
-class Solution:
-    # @return an integer
+    
     def reverse(self, x):
         result = 0
-
         if x < 0:
             symbol = -1
             x = -x
         else:
             symbol = 1
-
         while x:
             result = result * 10 + x % 10
             x /= 10
-
         return 0 if result > pow(2, 31) else result * symbol
