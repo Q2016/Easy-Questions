8391bc072e6b0edd895c92df986a95eba7d0804f
Q2016 <k.sohraby@gmail.com>
2022-02-28 13:03:05 -0800

    Update 734. Sentence Similarity.py

--- 734. Sentence Similarity.py
+++ 734. Sentence Similarity.py
@@ -4,26 +4,13 @@ arr = ["I","am",happy","with","leetcode"].
 Given two sentences sentence1 and sentence2 each represented as a string array and given an array of string pairs similarPairs where 
 similarPairs[i] = [xi, yi] indicates that the two words xi and yi are similar.
 Return true if sentence1 and sentence2 are similar, or false if they are not similar.
-Two sentences are similar if:
-They have the same length (i.e. the same number of words)
-sentence1[i] and sentence2[i] are similar.
-Notice that a word is always similar to itself, also notice that the similarity relation is not transitive. For example, if the words a and b 
-are similar and the words b and c are similar, a and c are not necessarily similar.
+Two sentences are similar if: They have the same length, sentence1[i] and sentence2[i] are similar.
 
 Example 1:
 Input: sentence1 = ["great","acting","skills"], sentence2 = ["fine","drama","talent"], similarPairs = [["great","fine"],["drama","acting"],["skills","talent"]]
 Output: true
 Explanation: The two sentences have the same length and each word i of sentence1 is also similar to the corresponding word in sentence2.
 
-Example 2:
-Input: sentence1 = ["great"], sentence2 = ["great"], similarPairs = []
-Output: true
-Explanation: A word is similar to itself.
-
-Example 3:
-Input: sentence1 = ["great"], sentence2 = ["doubleplus","good"], similarPairs = [["great","doubleplus"]]
-Output: false
-Explanation: As they don't have the same length, we return false.
 
        
 Solution:
