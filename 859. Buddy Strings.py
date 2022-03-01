989679d500c25da30d0c20dd9dd7cf79d7d4d43e
Q2016 <k.sohraby@gmail.com>
2022-02-28 16:28:15 -0800

    Update 859. Buddy Strings.py

--- 859. Buddy Strings.py
+++ 859. Buddy Strings.py
@@ -8,11 +8,15 @@ Input: s = "ab", goal = "ba"
 Output: true
 Explanation: You can swap s[0] = 'a' and s[1] = 'b' to get "ba", which is equal to goal.
 
+ 
+ 
+ 
 Solution:
 If the characters at the index of i in both strings are identical, i.e. s[i] == goal[i], we call the characters at the index i as matched.
 If swapping s[i] and s[j] would demonstrate that s and goal are buddy strings, then s[i] == goal[j] and s[j] == goal[i]. That means among the four 
 free variables s[i], s[j], goal[i], goal[j], there are only two cases: either s[i] == s[j] or not.
-  
+ 
+ 
   class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
         
@@ -34,3 +38,6 @@ free variables s[i], s[j], goal[i], goal[j], there are only two cases: either s[
             if len(pairs) >= 3: 
                 return False
         return len(pairs) == 2 and pairs[0] == pairs[1][::-1]
+       
+Or we can just use: 
+1- Use counter and compare the results for two strings 2- count the number of pairs that are not match, if more than two pairs=> False
