a51dcbad38f3fd83c1ea4be854379376e3e8c708
Q2016 <k.sohraby@gmail.com>
2022-02-28 16:16:27 -0800

    Update 852. Peak Index in a Mountain Array.py

--- 852. Peak Index in a Mountain Array.py
+++ 852. Peak Index in a Mountain Array.py
@@ -4,12 +4,12 @@ arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 
                                                                                               /    \
 
 
-Solution: Linear Scan
+Solution: Linear Scan, look at the 'if' condition
 
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         n = len(arr)
         for i in range(1, n-1):
-            if arr[i-1] < arr[i] > arr[i+1]:
+            if arr[i-1] < arr[i] > arr[i+1]: # we can use multiple conditions in python
                 return i
 
             
