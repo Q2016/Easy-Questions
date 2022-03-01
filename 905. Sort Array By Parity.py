0fc2d90d68a4813a6c19a0884e3300645d91a74c
Q2016 <k.sohraby@gmail.com>
2022-02-28 16:59:07 -0800

    Update 905. Sort Array By Parity.py

--- 905. Sort Array By Parity.py
+++ 905. Sort Array By Parity.py
@@ -7,7 +7,9 @@ Input: nums = [3,1,2,4]
 Output: [2,4,3,1]
 Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
 
-Solution:
+    
+    
+Solution: Teaching how to use alt-sort in python
 class Solution(object):
     def sortArrayByParity(self, A):
         A.sort(key = lambda x: x % 2)
