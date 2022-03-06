0984a59a67709174c4872582d4002fa219b6c19a
Q2016 <k.sohraby@gmail.com>
2022-03-05 20:16:21 -0800

    Update 100. Same Tree.py

--- 100. Same Tree.py
+++ 100. Same Tree.py
@@ -2,6 +2,9 @@ Question:
 Given the roots of two binary trees p and q, write a function to check if they are the same or not.
 Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
 
+
+
+
 Solution: Recursive
   
 class Solution:
