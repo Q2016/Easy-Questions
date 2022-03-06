59994df5f303f8a245bd6bb65de1526458c29daf
Q2016 <k.sohraby@gmail.com>
2022-03-05 20:28:55 -0800

    Update 1207. Unique Number of Occurrences.py

--- 1207. Unique Number of Occurrences.py
+++ 1207. Unique Number of Occurrences.py
@@ -7,6 +7,10 @@ Output: true
 Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
 
 
+    
+    
+    
+    
 Solution:
 Count the occurrences of each char; Compare if the numbers of distinct chars and distinct counts are equal.
 
