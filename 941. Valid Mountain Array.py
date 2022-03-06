99e4aa7e13140c518361d7d72378c5f232f778a2
Q2016 <k.sohraby@gmail.com>
2022-03-05 21:10:19 -0800

    Update 941. Valid Mountain Array.py

--- 941. Valid Mountain Array.py
+++ 941. Valid Mountain Array.py
@@ -1,12 +1,14 @@
 Question:
 Given an array of integers arr, return true if and only if it is a valid mountain array.
-
 Recall that arr is a mountain array if and only if:
-
 arr.length >= 3
 There exists some i with 0 < i < arr.length - 1 such that:
 arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
 arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+
+
+
 
 Solution:One Pass (This is question 852 i.e. repetitative)
 
@@ -14,6 +16,7 @@ If we walk along the mountain from left to right, we have to move strictly up, t
 Algorithm
 Let's walk up from left to right until we can't: that has to be the peak. We should ensure the peak is not the first or last element. 
 Then, we walk down. If we reach the end, the array is valid, otherwise its not.
+
 
 class Solution(object):
     def validMountainArray(self, A):
