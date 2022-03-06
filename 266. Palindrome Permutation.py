14b585f3fbaf5d591a3b3329418ef8fdc70ba68c
Q2016 <k.sohraby@gmail.com>
2022-03-05 20:40:41 -0800

    Update 266. Palindrome Permutation.py

--- 266. Palindrome Permutation.py
+++ 266. Palindrome Permutation.py
@@ -6,6 +6,12 @@ For example, "code" -> False, "aab" -> True, "carerac" -> True.
 
 
 
+
+
+
+
+
+
 Hint:
 Consider the palindromes of odd vs even length. What difference do you notice? Count the frequency of each character. 
 If each character occurs even number of times, then it must be a palindrome. How about character which occurs odd number of times?
