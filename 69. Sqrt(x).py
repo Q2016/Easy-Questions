1c0820c76d7f1e3ad92afe3e2ebbacbe0c20689f
Q2016 <k.sohraby@gmail.com>
2022-03-05 21:06:14 -0800

    Update 69. Sqrt(x).py

--- 69. Sqrt(x).py
+++ 69. Sqrt(x).py
@@ -5,6 +5,12 @@ Since the return type is an integer, the decimal digits are truncated, and only 
 
 Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
 
+    
+    
+    
+    
+    
+    
 Solution: BST from 278
 
 # newton method
