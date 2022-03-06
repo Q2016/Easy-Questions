2a50ce12896729f67dc65ba19758dbf6ec68050f
Q2016 <k.sohraby@gmail.com>
2022-03-05 20:40:32 -0800

    Update 257. Binary Tree Paths.py

--- 257. Binary Tree Paths.py
+++ 257. Binary Tree Paths.py
@@ -2,6 +2,21 @@ Question:
 Given the root of a binary tree, return all root-to-leaf paths in any order. A leaf is a node with no children.
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 Solution:
 # dfs top down + stack
     def binaryTreePaths1(self, root):
