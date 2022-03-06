07209946e55f63ad2ab503dd72a0a0e41731f82b
Q2016 <k.sohraby@gmail.com>
2022-03-05 20:28:42 -0800

    Update 118. Pascal's Triangle.py

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -6,6 +6,15 @@ Example 1:
 Input: numRows = 5
 Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
 Solution:
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
