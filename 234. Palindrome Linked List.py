a0563bf5909a5cd88fc0bf009f67fdd4d3741613
Q2016 <k.sohraby@gmail.com>
2022-03-07 14:56:58 -0800

    Update 234. Palindrome Linked List.py

--- 234. Palindrome Linked List.py
+++ 234. Palindrome Linked List.py
@@ -16,18 +16,54 @@ Given the head of a singly linked list, return true if it is a palindrome.
 
 
 Solution:
-Phase 1: Reverse the first half while finding the middle.
-Phase 2: Compare the reversed first half with the second half.
 
-def isPalindrome(self, head):
-    rev = None
-    slow = fast = head
-    while fast and fast.next:
-        fast = fast.next.next
-        rev, rev.next, slow = slow, rev, slow.next
-    if fast:
-        slow = slow.next
-    while rev and rev.val == slow.val:
-        slow = slow.next
-        rev = rev.next
-    return not rev
+This can be solved by reversing the 2nd half and compare the two halves. Let's start with an example [1, 1, 2, 1].
+
+In the beginning, set two pointers fast and slow starting at the head.
+
+1 -> 1 -> 2 -> 1 -> null 
+sf
+(1) Move: fast pointer goes to the end, and slow goes to the middle.
+
+1 -> 1 -> 2 -> 1 -> null 
+          s          f
+(2) Reverse: the right half is reversed, and slow pointer becomes the 2nd head.
+
+1 -> 1    null <- 2 <- 1           
+h                      s
+(3) Compare: run the two pointers head and slow together and compare.
+
+1 -> 1    null <- 2 <- 1             
+     h            s
+public boolean isPalindrome(ListNode head) {
+    ListNode fast = head, slow = head;
+    while (fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    if (fast != null) { // odd nodes: let right half smaller
+        slow = slow.next;
+    }
+    slow = reverse(slow);
+    fast = head;
+    
+    while (slow != null) {
+        if (fast.val != slow.val) {
+            return false;
+        }
+        fast = fast.next;
+        slow = slow.next;
+    }
+    return true;
+}
+
+public ListNode reverse(ListNode head) {
+    ListNode prev = null;
+    while (head != null) {
+        ListNode next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
+}
