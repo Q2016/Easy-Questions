40558d2432d3021ab9fd4536af41cf209fdcfee8
Q2016 <k.sohraby@gmail.com>
2022-03-07 08:04:48 -0800

    Update 171. Excel Sheet Column Number.py

--- 171. Excel Sheet Column Number.py
+++ 171. Excel Sheet Column Number.py
@@ -9,7 +9,13 @@ Z -> 26
 AA -> 27
 AB -> 28 
 
+
+
+
 Solution:
+    
+Going through the digits from right to left i.e first, second, third each must be multiplied by 26 similar to 10 based numbers that 123= 100*1+10*2+1*3
+
 def titleToNumber(self, s):
     res = 0
     for c in s:
