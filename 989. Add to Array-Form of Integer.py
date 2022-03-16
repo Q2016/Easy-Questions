a547f6caa20e86b1a821443b34be95af3196240d
Q2016 <k.sohraby@gmail.com>
2022-03-16 15:51:52 -0700

    Update 989. Add to Array-Form of Integer.py

--- 989. Add to Array-Form of Integer.py
+++ 989. Add to Array-Form of Integer.py
@@ -23,7 +23,7 @@ class Solution(object):
     def addToArrayForm(self, A, K):
         A[-1] += K
         for i in xrange(len(A) - 1, -1, -1):
-            carry, A[i] = divmod(A[i], 10)
+            carry, A[i] = divmod(A[i], 10) # If x and y are integers, the return value from divmod() is same as (a // b, x % y)
             if i: A[i-1] += carry
         if carry:
             A = map(int, str(carry)) + A
