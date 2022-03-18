65c82627bc2a41f8d806924853516e4f523a9535
Q2016 <k.sohraby@gmail.com>
2022-03-18 10:06:04 -0700

    Update 687. Longest Univalue Path.py

--- 687. Longest Univalue Path.py
+++ 687. Longest Univalue Path.py
@@ -19,21 +19,28 @@ the children node value. In the Diameter of Binary Tree question, the path can e
 Hence at the end of each recursive loop, return the longest length using that node as the root so that the node's parent can potentially use it 
 in its longest path computation. We also use an external variable longest that keeps track of the longest path seen so far.
 
+class Solution(object):
     def longestUnivaluePath(self, root):
-        longest = [0]
-        def traverse(node):
-            if not node:
-                return 0
-            left_len, right_len = traverse(node.left), traverse(node.right)
-            left = (left_len + 1) if node.left and node.left.val == node.val else 0
-            right = (right_len + 1) if node.right and node.right.val == node.val else 0
-            longest[0] = max(longest[0], left + right)
-            return max(left, right)
-        traverse(root)
-        return longest[0]
+        self.ans = 0
+
+        def arrow_length(node):
+            if not node: return 0
+            left_length = arrow_length(node.left)
+            right_length = arrow_length(node.right)
+            left_arrow = right_arrow = 0
+            if node.left and node.left.val == node.val:
+                left_arrow = left_length + 1
+            if node.right and node.right.val == node.val:
+                right_arrow = right_length + 1
+            self.ans = max(self.ans, left_arrow + right_arrow)
+            return max(left_arrow, right_arrow)
+
+        arrow_length(root)
+        return self.ans
     
     
     
-Time: O(n)
-Space: O(n)
+Complexity Analysis
+Time Complexity: O(N), where N is the number of nodes in the tree. We process every node once.
+Space Complexity: O(H), where H is the height of the tree. Our recursive call stack could be up to H layers deep.
     
