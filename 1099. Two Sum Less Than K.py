795debeeadba11d7c7d67f203dab22e904b00f2a
Q2016 <k.sohraby@gmail.com>
2022-03-21 10:15:05 -0700

    Update 1099. Two Sum Less Than K.py

--- 1099. Two Sum Less Than K.py
+++ 1099. Two Sum Less Than K.py
@@ -19,18 +19,17 @@ class Solution:
     def twoSumLessThanK(self, nums: List[int], k: int) -> int:
         
         nums = sorted(nums)
-        i = 0
-        j = len(nums) - 1
-        
         max_sum = -1
         
-        while i < j:
-            if nums[i] + nums[j] >= k:
-                j -= 1
-            else:
-                max_sum = max(max_sum, nums[i] + nums[j])
-                i += 1
+        left = 0
+        right = len(nums) - 1
         
+        while left < right:
+            if nums[left] + nums[right] >= k:
+                right -= 1
+            else:
+                max_sum = max(max_sum, nums[left] + nums[right])
+                left += 1
         return max_sum
         
         
