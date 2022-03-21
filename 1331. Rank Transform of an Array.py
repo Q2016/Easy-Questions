229144898e6fe4e7e288bd539db7e0e09bd3fb17
Q2016 <k.sohraby@gmail.com>
2022-03-21 08:20:10 -0700

    Update 1331. Rank Transform of an Array.py

--- 1331. Rank Transform of an Array.py
+++ 1331. Rank Transform of an Array.py
@@ -10,7 +10,10 @@ Input: arr = [40,10,20,30]
 Output: [4,1,2,3]
 Explanation: 40 is the largest element. 10 is the smallest. 20 is the second smallest. 30 is the third smallest.
 
-Solution:
+ 
+ 
+Solution: Dic mapping (in a way educational)
+ 
 Remove duplicates with set, then sort, then build a dict mapping values to ranks, then use that dict on the given list.
 
 Time: O(n log n)
