60be0ce3dccd0f7825d10fd93c3bd6b16fa432e1
Q2016 <k.sohraby@gmail.com>
2022-03-22 10:52:03 -0700

    Update 687. Longest Univalue Path.py

--- 687. Longest Univalue Path.py
+++ 687. Longest Univalue Path.py
@@ -12,8 +12,21 @@ Output: 2
     
     
     
-Solution: DFS, Similar to Diameter of Binary Tree
+Solution: DFS, Similar to 543. Diameter of Binary Tree
+I added the solution of 543 below:
+   def diameterOfBinaryTree(self, root):
+        self.ans = 0
+        def depth(p):
+            if not p: return 0
+            left, right = depth(p.left), depth(p.right)
+            self.ans = max(self.ans, left+right)
+            return 1 + max(left, right)
+            
+        depth(root)
+        return self.ans
+
     
+Original solution to 687:    
 The approach is similar to the Diameter of Binary Tree question except that we reset the left/right to 0 whenever the current node does not match 
 the children node value. In the Diameter of Binary Tree question, the path can either go through the root or it doesn't.
 Hence at the end of each recursive loop, return the longest length using that node as the root so that the node's parent can potentially use it 
