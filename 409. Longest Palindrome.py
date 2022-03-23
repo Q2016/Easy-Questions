59e8c1ad46c65dcd4f05471f0e97a00985679ee6
Q2016 <k.sohraby@gmail.com>
2022-03-22 17:39:08 -0700

    Update 409. Longest Palindrome.py

--- 409. Longest Palindrome.py
+++ 409. Longest Palindrome.py
@@ -13,22 +13,22 @@ One longest palindrome that can be built is "dccaccd", whose length is 7.
 
 
 
-Solution: Greedy
+Solution: Greedy (Do you understand this?)
+  
 A palindrome consists of letters with equal partners, plus possibly a unique center (without a partner). The letter i from the left 
 has its partner i from the right. For example in 'abcba', 'aa' and 'bb' are partners, and 'c' is a unique center.
 Imagine we built our palindrome. It consists of as many partnered letters as possible, plus a unique center if possible. This motivates a greedy approach.
-
 For each letter, say it occurs v times. We know we have v // 2 * 2 letters that can be partnered for sure. For example, if we have 'aaaaa', 
 then we could have 'aaaa' partnered, which is 5 // 2 * 2 = 4 letters partnered.
 At the end, if there was any v % 2 == 1, then that letter could have been a unique center. Otherwise, every letter was partnered. To perform 
 this check, we will check for v % 2 == 1 and ans % 2 == 0, the latter meaning we haven't yet added a unique center to the answer.
   
-class Solution:
+
     def longestPalindrome(self, s):
         ans = 0
-        for v in collections.Counter(s).itervalues():
-            ans += v // 2 * 2
-            if ans % 2 == 0 and v % 2 == 1:
+        for f in collections.Counter(s).itervalues(): # I use f for frequency
+            ans += f // 2 * 2
+            if ans % 2 == 0 and v % 2 == 1: # This condition means f was odd, so we add +1 because ans is always an even number
                 ans += 1
         return ans  
       
