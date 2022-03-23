6c5538fb23c17d67200f82be0f46fbb17d42000b
Q2016 <k.sohraby@gmail.com>
2022-03-23 08:41:29 -0700

    Update 204. Count Primes.py

--- 204. Count Primes.py
+++ 204. Count Primes.py
@@ -12,14 +12,15 @@ Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
   
 
 Solution:
-Animation: https://leetcode.com/problems/count-primes/discuss/1200796/JS-Python-Java-C%2B%2B-or-Easy-Sieve-of-Eratosthenes-Solution-w-Explanation    
+
+Animation: 
+https://leetcode.com/problems/count-primes/discuss/1200796/JS-Python-Java-C%2B%2B-or-Easy-Sieve-of-Eratosthenes-Solution-w-Explanation    
   
-There are several ways to go about solving this problem, but the classic solution is known as the sieve of Eratosthenes. For the sieve of Eratosthenes, 
-we start by creating a boolean array (seen) of size n to represent each of the numbers less than n.
-We start at 2 and for each number processed (num), we iterate through and mark each multiple (mult) of num, starting at num^2, as seen. We start at num^2 
-because every multiple up to the num'th multiple will have been guaranteed to have been seen before, since they're also a multiple of a smaller number. 
+The sieve of Eratosthenes, we start by creating a boolean array (seen) of size n to represent each of the numbers less than n.
+We start at 2 and for each number processed, we iterate through and mark each multiple of n, starting at n^2, as seen. We start at n^2 
+because every multiple up to the n'th multiple will have been guaranteed to have been seen before, since they're also a multiple of a smaller number. 
 For example, when processing 5s, we can skip to 25 because 10 will have been seen when we processed 2s, 15 when we processed 3s, and 20 when we processed 2s.
-Then we move num forward, skipping any numbers that have already been seen. By doing this, we will only stop on prime numbers, because they haven't 
+Then we move nu forward, skipping any numbers that have already been seen. By doing this, we will only stop on prime numbers, because they haven't 
 been seen as a multiple of a previous iteration. We just have to update our count (ans) each time we stop and then return ans once we reach n.
 
 
@@ -29,9 +30,9 @@ been seen as a multiple of a previous iteration. We just have to update our coun
         if n <= 2:
             return 0
         res = [True] * n
-        res[0] = res[1] = False
+        res[0] = res[1] = False # 0 and 1 are not prime
         for i in range(2, n):
-            if res[i] == True:
+            if res[i] == True: # if 'i' is a prime, search from i to i^2 
                 for j in range(i, int((n-1)/i+1)): # look at the index below, (n-1)/i+1 is fixed by num^2 condition mentioned above
                     res[i*j] = False
         return sum(res)
