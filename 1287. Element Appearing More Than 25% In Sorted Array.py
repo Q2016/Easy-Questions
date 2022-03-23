553c895110cce23490bf0e98e52f7ffe01832d3d
Q2016 <k.sohraby@gmail.com>
2022-03-23 07:39:15 -0700

    Update 1287. Element Appearing More Than 25% In Sorted Array.py

--- 1287. Element Appearing More Than 25% In Sorted Array.py
+++ 1287. Element Appearing More Than 25% In Sorted Array.py
@@ -17,8 +17,34 @@ Output: 6
   
   
 Solution:
+
+The solution for this problem is based on the array being sorted, in the un-sorted array, we need to use 169. Majority Element: Boyer-Moore Majority Vote
+  
   def findSpecialInteger(self, arr: List[int]) -> int:
       n = len(arr) // 4
       for i in range(len(arr)):
           if arr[i] == arr[i + n]:
             return arr[i]
+
+          
+          
+169. Majority Element: Boyer-Moore Majority Vote  
+  
+from collections import defaultdict
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        majority = 0
+        for num in nums:
+            if count == 0:
+                majority = num
+                count += 1
+            elif num == majority:
+                count += 1
+            else:
+                count -= 1
+        return majority
+
+Complexity
+Time is O(n)
+Space is O(1)          
