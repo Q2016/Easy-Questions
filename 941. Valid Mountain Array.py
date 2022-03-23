8693e7361d81f0f5906cd9e93b01c75cb8a8f3b0
Q2016 <k.sohraby@gmail.com>
2022-03-23 08:20:25 -0700

    Update 941. Valid Mountain Array.py

--- 941. Valid Mountain Array.py
+++ 941. Valid Mountain Array.py
@@ -1,10 +1,8 @@
 Question:
 Given an array of integers arr, return true if and only if it is a valid mountain array.
-Recall that arr is a mountain array if and only if:
-arr.length >= 3
-There exists some i with 0 < i < arr.length - 1 such that:
-arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
-arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+Recall that arr is a mountain array if and only if: arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:          arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+                                                                    arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
 
 
 
@@ -12,6 +10,8 @@ arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
 
 
 
+        
+        
 
 Solution:One Pass (This is question 852 i.e. repetitative)
 
