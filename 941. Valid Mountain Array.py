23449a1a7cdbe7a34acaa9ecccf9a13e398226b5
Q2016 <k.sohraby@gmail.com>
2022-03-23 08:18:41 -0700

    Update 941. Valid Mountain Array.py

--- 941. Valid Mountain Array.py
+++ 941. Valid Mountain Array.py
@@ -12,6 +12,7 @@ arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
 
 
 
+
 Solution:One Pass (This is question 852 i.e. repetitative)
 
 If we walk along the mountain from left to right, we have to move strictly up, then strictly down.
@@ -24,17 +25,29 @@ class Solution(object):
     def validMountainArray(self, A):
         N = len(A)
         i = 0
-
         # walk up
         while i+1 < N and A[i] < A[i+1]:
             i += 1
-
         # peak can't be first or last
         if i == 0 or i == N-1:
             return False
-
         # walk down
         while i+1 < N and A[i] > A[i+1]:
             i += 1
-
         return i == N-1
+    
+Similar question is 852. Peak Index in a Mountain Array:
+
+    Linear Scan, look at the 'if' condition
+
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        n = len(arr)
+        for i in range(1, n-1):
+            if arr[i-1] < arr[i] > arr[i+1]: # we can use multiple conditions in python
+                return i
+
+            
+Complexity:
+Time: O(N)
+Space: O(1)
+    
