a7b2f1cb343402048263ad232c3889df729bc7f0
Q2016 <k.sohraby@gmail.com>
2022-06-05 14:23:02 -0700

    Update 293*. Flip Game.py

--- 293*. Flip Game.py
+++ 293*. Flip Game.py
@@ -11,20 +11,26 @@ Input: currentState = "++++"
 Output: ["--++","+--+","++--"]
 
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 Solution:
 
 class Solution:
     def generatePossibleNextMoves(self, currentState: str) -> List[str]:
         results = []
-
         for i in range(0, len(currentState) - 1):
             sub = currentState[i:i+2]            
-
             if sub == "++":
                 flip = currentState[:i] + "--" + currentState[i+2:]
                 results.append(flip)
-
 
         return results
         
