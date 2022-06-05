df0ce67e2e732b5b7c6f9458921162a8aa4d45f6
Q2016 <k.sohraby@gmail.com>
2022-06-05 13:37:51 -0700

    Update 26*. Remove Duplicates from Sorted Array.py

--- 26*. Remove Duplicates from Sorted Array.py
+++ 26*. Remove Duplicates from Sorted Array.py
@@ -6,34 +6,27 @@ the first k elements of nums should hold the final result. It does not matter wh
 Return k after placing the final result in the first k slots of nums. Do not allocate extra space for another array. You must do this by modifying 
 the input array in-place with O(1) extra memory.
 
-Custom Judge:
-The judge will test your solution with the following code:
-int[] nums = [...]; // Input array
-int[] expectedNums = [...]; // The expected answer with correct length
-int k = removeDuplicates(nums); // Calls your implementation
-assert k == expectedNums.length;
-for (int i = 0; i < k; i++) {
-    assert nums[i] == expectedNums[i];
-}
-If all assertions pass, then your solution will be accepted.
-
 Example 1:
 Input: nums = [1,1,2]
 Output: 2, nums = [1,2,_]
 Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
 
-Solution:
-Given a sorted array and we need to return the length of the unique elements instead of the entire array. There is no need to delete the duplicate elements also.
 
-Since, our first element is already present at index 0 (it is a unique element), we quickly run a for loop for the entire array to scan for unique elements.
-If the current element and the next element are the same, then we just keep on going till we find a different element
-Once we find a different element, it is inserted at index 1, because, index 0 is taken by the first unique element.
-Once this is done, the same scanning is done to find out the next unique element and this element is to be inserted at index 2. This process continues until we are done with unique elements.
-We use a variable (x = 1) which is incremented to the next index whenever we find a unique element and we insert this element at its corresponding index.
-x = 1
-for i in range(len(nums)-1):
-	if(nums[i]!=nums[i+1]):
-		nums[x] = nums[i+1]
-		x+=1
-return(x)
+
+
+
+
+
+
+
+
+Solution:
+
+def removeDuplicates(nums):	
+	x = 1
+	for i in range(len(nums)-1):
+		if(nums[i]!=nums[i+1]):
+			nums[x] = nums[i+1]
+			x+=1
+	return nums
