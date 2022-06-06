7d99ae8c8e009c4118fc79fe7cf0ef6ec3a1a77c
Q2016 <k.sohraby@gmail.com>
2022-06-05 21:36:35 -0700

    Update 709*. To Lower Case.py

--- 709*. To Lower Case.py
+++ 709*. To Lower Case.py
@@ -5,13 +5,26 @@ Example 1:
 Input: s = "Hello"
 Output: "hello"
 
+    
+    
+    
+    
+    
+    
+    
+    
+  
+
+
+
+
 Solution:
+    
 class Solution:
     def toLowerCase(self, str): 
         return "".join(chr(ord(c) + 32) if 65 <= ord(c) <= 90 else c for c in str)
+
 class Solution:
     def toLowerCase(self, str): 
         return "".join(chr(ord(c) + 32) if "A" <= c <= "Z" else c for c in str)
-class Solution:
-    def toLowerCase(self, str): 
-        return str.lower()
+
