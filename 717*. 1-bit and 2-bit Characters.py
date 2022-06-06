706c9a64a5d5fd3c707fd282f692cc8e3040998b
Q2016 <k.sohraby@gmail.com>
2022-06-06 08:10:02 -0700

    Update 717*. 1-bit and 2-bit Characters.py

--- 717*. 1-bit and 2-bit Characters.py
+++ 717*. 1-bit and 2-bit Characters.py
@@ -6,30 +6,51 @@ the 0 bit representation (means it's not valid when the last 0 is translated int
 Example 1:
 Input: bits = [1,0,0]
 Output: true
-Explanation: The only way to decode it, is two-bit character and one-bit character.
+Explanation: The only way to decode it is two-bit character and one-bit character.
 So the last character is one-bit character.
+Example 2:
+Input: bits = [1,1,1,0]
+Output: false
+Explanation: The only way to decode it is two-bit character and two-bit character.
+So the last character is not one-bit character.
 
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
+                          
 
 Solution:
+                          
+if we iterate through from left->right
+      for each of the bit 'b' at index 'i'
+             if 'b' is equal to 1
+                  it must be 2 bits
+                        then we can move to index 'i+2'
+             else 'b' is equal to 0
+                 it must be 1 bit
+                      then we can move to index 'i+1'
+      After performing the iteration
+           if our last index is at the end of the array
+                  Then it must be a 1 bit (0)
+                          
+                          
+                          
 # (Time, Space) Complexity : O(n), O(1)
 
     def isOneBitCharacter(self, bits: List[int]) -> bool:
-        # Important Rules:
-        # 1. If bit n is 0, bit n+1 must be a new char
-        # 2. If bits end with 1, last bit must be a two bit char
-        #    However, this case had been rejected by question
-        # 3. If 1s in row and end with 0, 
-        #    we can use count or 1s to check last char
-        #    If count is even, last char is "0"
-        #    If count is odd,  last char is "10"
-        # Strategy:
-        # 1. We don't care last element, since it must be 0.
-        # 2. We check from reversed, and count 1s in a row
-        # 3. Once 0 occur or list end, We stop counting
-        # 4. We use count to determin result
-        # 5. Since we will mod count by 2, we simplify it to bool
-        ret = True
-        for bit in bits[-2::-1]:
-            if bit: ret = not ret
-            else: break
-        return ret
+           if not bits: return False
+            n = len(bits)
+
+            index = 0
+            while index < n:
+                if index == n-1 : return True
+                if bits[index] == 1: 
+                    index += 2              
+                else: index += 1
+            return False
