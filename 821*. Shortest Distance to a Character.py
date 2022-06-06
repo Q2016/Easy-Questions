c84f3e41448b850855b66f263e979cec9c555fc9
Q2016 <k.sohraby@gmail.com>
2022-06-06 15:39:38 -0700

    Update 821*. Shortest Distance to a Character.py

--- 821*. Shortest Distance to a Character.py
+++ 821*. Shortest Distance to a Character.py
@@ -6,14 +6,23 @@ The distance between two indices i and j is abs(i - j), where abs is the absolut
 Example 1:
 Input: s = "loveleetcode", c = "e"
 Output: [3,2,1,0,1,0,0,1,2,2,1,0]
-Explanation: The character 'e' appears at indices 3, 5, 6, and 11 (0-indexed).
-The closest occurrence of 'e' for index 0 is at index 3, so the distance is abs(0 - 3) = 3.
-The closest occurrence of 'e' for index 1 is at index 3, so the distance is abs(1 - 3) = 2.
-For index 4, there is a tie between the 'e' at index 3 and the 'e' at index 5, but the distance is still the same: abs(4 - 3) == abs(4 - 5) = 1.
-The closest occurrence of 'e' for index 8 is at index 6, so the distance is abs(8 - 6) = 2.
 
-Solution:
-For each index S[i], let's try to find the distance to the next character C going left, and going right. The answer is the minimum of these two values.
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+Solution:Min Array
+For each index S[i], let's try to find the distance to the next character C going left, and going right. 
+The answer is the minimum of these two values.
 When going left to right, we'll remember the index prev of the last character C we've seen. Then the answer is i - prev.
 When going right to left, we'll remember the index prev of the last character C we've seen. Then the answer is prev - i.
 We take the minimum of these two answers to create our final answer.
@@ -32,3 +41,11 @@ class Solution(object):
             ans[i] = min(ans[i], prev - i)
 
         return ans
+
+    
+    
+Complexity Analysis
+
+Time Complexity: O(N), where N is the length of S. We scan through the string twice.
+
+Space Complexity: O(N), the size of ans.    
