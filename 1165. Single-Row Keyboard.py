755d6a0cd2a842a3c01fc9854150ea29035ffff9
Q2016 <k.sohraby@gmail.com>
2022-06-07 19:33:06 -0700

    Update 1165. Single-Row Keyboard.py

--- 1165. Single-Row Keyboard.py
+++ 1165. Single-Row Keyboard.py
@@ -1,8 +1,8 @@
 Question:
-There is a special keyboard with all keys in a single row. Given a string keyboard of length 26 indicating the layout of the keyboard 
-(indexed from 0 to 25), initially your finger is at index 0. To type a character, you have to move your finger to the index of the 
+Given a string keyboard of length 26 indicating the layout of the keyboard, initially your finger is at index 0. 
+To type a character, you have to move your finger to the index of the 
 desired character. The time taken to move your finger from index i to index j is |i - j|.
-You want to type a string word. Write a function to calculate how much time it takes to type it with one finger.
+You want to type a string 'word'. Write a function to calculate how much time it takes to type it with one finger.
 
 Example 1:
 Input: keyboard = "abcdefghijklmnopqrstuvwxyz", word = "cba"
