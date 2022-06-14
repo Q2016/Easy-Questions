1eeb9e584a7fae40640ff4f66b3391885079484e
Q2016 <k.sohraby@gmail.com>
2022-06-14 11:21:34 -0700

    Update 849. Maximize Distance to Closest Person.py

--- 849. Maximize Distance to Closest Person.py
+++ 849. Maximize Distance to Closest Person.py
@@ -14,28 +14,69 @@ Thus, the maximum distance to the closest person is 2.
 
 
 
-Solution: One pass (I think this solution has some issues, we dont need *** below)
-How to find the max distance between two 1s. 
+Solution: Approach #1: Next Array [Accepted]
+Intuition
 
-    def maxDistToClosest(seats) :
-        longest = 0 
-        beginning = 0 
-        i = 0
-        # count zeros in beginning
-        while (seats[i]==0): # ***
-            beginning++
-            i++
-        # find longest sequence of zeros in middle
-        count = beginning
-        while i<len(seats):
-            if (seats[i] == 0):
-                count++
-            else:
-                longest = max(count, longest)
-                count = 0
-        # at the end, count = length of zeros in the end
-        # in the middle we have to divide the length in two to find maximum distance
-        longest = longest/2 if longest % 2 == 0 else longest/2+1
-        # return the largest distance
-        return max(longest, count, beginning)
+Let left[i] be the distance from seat i to the closest person sitting to the left of i. Similarly, let right[i] be the distance to 
+the closest person sitting to the right of i. This is motivated by the idea that the closest person in seat i sits a distance 
+min(left[i], right[i]) away.
+
+Algorithm
+
+To construct left[i], notice it is either left[i-1] + 1 if the seat is empty, or 0 if it is full. right[i] is constructed in a similar way.
     
+class Solution(object):
+    def maxDistToClosest(self, seats):
+        N = len(seats)
+        left, right = [N] * N, [N] * N
+
+        for i in xrange(N):
+            if seats[i] == 1: left[i] = 0
+            elif i > 0: left[i] = left[i-1] + 1
+
+        for i in xrange(N-1, -1, -1):
+            if seats[i] == 1: right[i] = 0
+            elif i < N-1: right[i] = right[i+1] + 1
+
+        return max(min(left[i], right[i])
+                   for i, seat in enumerate(seats) if not seat)
+    
+    
+Complexity Analysis
+
+Time Complexity: O(N), where N is the length of seats.
+
+Space Complexity: O(N), the space used by left and right. 
+    
+
+Approach #2: Two Pointer [Accepted]
+Intuition
+
+As we iterate through seats, we'll update the closest person sitting to our left, and closest person sitting to our right.
+
+Algorithm
+
+Keep track of prev, the filled seat at or to the left of i, and future, the filled seat at or to the right of i.
+
+Then at seat i, the closest person is min(i - prev, future - i), with one exception. i - prev should be considered infinite 
+if there is no person to the left of seat i, and similarly future - i is infinite if there is no one to the right of seat i.    
+
+
+class Solution(object):
+    def maxDistToClosest(self, seats):
+        people = (i for i, seat in enumerate(seats) if seat)
+        prev, future = None, next(people)
+
+        ans = 0
+        for i, seat in enumerate(seats):
+            if seat:
+                prev = i
+            else:
+                while future is not None and future < i:
+                    future = next(people, None)
+
+                left = float('inf') if prev is None else i - prev
+                right = float('inf') if future is None else future - i
+                ans = max(ans, min(left, right))
+
+        return ans
