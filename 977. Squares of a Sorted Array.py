40bc7cf63db1352218c56be3d553882d13155615
Q2016 <k.sohraby@gmail.com>
2022-06-14 13:05:10 -0700

    Update 977. Squares of a Sorted Array.py

--- 977. Squares of a Sorted Array.py
+++ 977. Squares of a Sorted Array.py
@@ -12,35 +12,30 @@ After sorting, it becomes [0,1,9,16,100].
 
 
 
-Solution: Two pointers (Crazy!)
+Solution: Two pointers
     
-The question boils down to understanding that if we look at the magnitude of the elements in the array at both ends and gradually 
-"slide down" and converge towards the center of the array. With that understanding, we can use two pointers, one at each end, 
-to iteratively collect the larger square to a list. However, collecting the larger square in a list with list's append, results 
-in elements sorted in descending order. To circumvent this, we need to append to the left of the list. Using a collections.deque() 
-allows us to append elements to the left of answer in O(1) time, maintaining the required increasing order.
+    https://www.youtube.com/watch?v=FPCZsG_AkUg
+        
+    - Find the lowest element and use it as the center
+    - Use right and left lements of this element as right and left pointers 
+    - scroll away from the center in both directions as you compare the abs() of left/right elements
 
-def sortedSquares(self, A):
-    answer = collections.deque()
-    l, r = 0, len(A) - 1
+    Alternatively, 
+        we can assign pointers to both ends of the array as below
+    
+    
+def sortedSquares(self, nums):
+    res=[]
+    l, r = 0, len(nums) - 1
     while l <= r:
-        left, right = abs(A[l]), abs(A[r])
-        if left > right:
-            answer.appendleft(left * left)
+        
+        if nums[l]*nums[l] > nums[r]*nums[r]:
+            res.append(nums[l]*nums[l])
             l += 1
         else:
-            answer.appendleft(right * right)
+            res.appendleft(nums[r]*nums[r])
             r -= 1
-    return list(answer)
+    return res[::-1] # reverse
 
 
-Or for O(nlog n): 
-    def sortedSquares(self, A: List[int]) -> List[int]:
-        for i in range(len(A)):
-            A[i] *= A[i]
-        A.sort()
-        return A    
 
-similarly:
-    def sortedSquares(self, A: List[int]) -> List[int]:
-        return sorted([v**2 for v in A])
