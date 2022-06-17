1b84ab05cb8beb1f04015527c7fb7e88bd484456
Q2016 <k.sohraby@gmail.com>
2022-06-17 13:13:44 -0700

    Update and rename 942. DI String Match.py to 942*. DI String Match.py

--- 942*. DI String Match.py
+++ 942*. DI String Match.py
@@ -10,7 +10,10 @@ Output: [0,4,1,3,2]
 
     
     
-Solution:
+    
+    
+Solution: (video link? is solution correct?)
+    
 If we see say S[0] == 'I', we can always put 0 as the first element; similarly, if we see S[0] == 'D', we can always put N as the first element.
 Say we have a match for the rest of the string S[1], S[2], ... using N distinct elements. Notice it doesn't matter what the elements are, 
 only that they are distinct and totally ordered. Then, putting 0 or N at the first character will match, and the rest of the elements 
