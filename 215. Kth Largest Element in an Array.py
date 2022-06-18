8f1e7e0bf859cc9f49543ab16ec948f9ff59da19
Q2016 <k.sohraby@gmail.com>
2022-06-18 08:20:29 -0700

    Update 215. Kth Largest Element in an Array.py

--- 215. Kth Largest Element in an Array.py
+++ 215. Kth Largest Element in an Array.py
@@ -29,7 +29,7 @@ def findKthLargest(self, nums, k):
         pivot, p=nums[r], l # p index of left, pivot rightmost element
         for i in range(l,r):
             if nums[i]<=pivot: # those that are smaller, put them in the back
-                nums[p], nums[i]=nums[i], nums[p]
+                nums[p], nums[i]=nums[i], nums[p] # this part is similar to bubble sort
                 p+=1
         nums[p], nums[r]=nums[r], nums[p] # the last swap to put pivot in the begining
 
@@ -84,31 +84,7 @@ def findKthLargest(self, nums, k):
 def findKthLargest5(self, nums, k):
     return heapq.nlargest(k, nums)[-1]
     
-# O(n) time, quick selection
-def findKthLargest(self, nums, k):
-    # convert the kth largest to smallest
-    return self.findKthSmallest(nums, len(nums)+1-k)
-    
-def findKthSmallest(self, nums, k):
-    if nums:
-        pos = self.partition(nums, 0, len(nums)-1)
-        if k > pos+1:
-            return self.findKthSmallest(nums[pos+1:], k-pos-1)
-        elif k < pos+1:
-            return self.findKthSmallest(nums[:pos], k)
-        else:
-            return nums[pos]
- 
-# choose the right-most element as pivot   
-def partition(self, nums, l, r):
-    low = l
-    while l < r:
-        if nums[l] < nums[r]:
-            nums[l], nums[low] = nums[low], nums[l]
-            low += 1
-        l += 1
-    nums[low], nums[r] = nums[r], nums[low]
-    return low
+
 
   
   
