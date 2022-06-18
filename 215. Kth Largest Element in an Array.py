db224e2e37ed4d5de7b2e6d8d0fedc75d36f8a9a
Q2016 <k.sohraby@gmail.com>
2022-06-17 20:08:40 -0700

    Update 215. Kth Largest Element in an Array.py

--- 215. Kth Largest Element in an Array.py
+++ 215. Kth Largest Element in an Array.py
@@ -12,41 +12,90 @@ Output: 5
   
   
 Solution:
-
-Pyhton:
-    def findKthLargest(self, nums, k):
-        return sorted(nums, reverse = True)[k-1]  
+  
+  https://www.youtube.com/watch?v=XEmy13g1Qxc
+  
+-One solution is to sort: NlogN  
+-one solution is to use maxheap: with heap we dont need to sort the array, turning array into a heap takes O(N) which give us the largest but since
+we want kth largest, so we have to pop k elements from the heap. Each pop takes logN and k times takes KlogN so overall the total complexity is N+KlogN  
+-One solution is quick select which is smilar to quick sort: O(N) 
+  we pick a pivot as a border and partition the array into two: less than the pivot number and greater
+  
+  
+  
+  
   
 
-C++:  
-  
-STL
-This problem needs to use partial sorting. In STL, there are two built-in functions (nth_element and partial_sort) for this.
 
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        nth_element(nums.begin(), nums.begin() + k - 1, nums.end(), greater<int>());
-        return nums[k - 1];
-    }
-};  
-  
 
-min-heap using priority_queue
+# O(nlgn) time
+def findKthLargest1(self, nums, k):
+    return sorted(nums, reverse=True)[k-1]
+    
+# O(nk) time, bubble sort idea, TLE
+def findKthLargest2(self, nums, k):
+    for i in xrange(k):
+        for j in xrange(len(nums)-i-1):
+            if nums[j] > nums[j+1]:
+                # exchange elements, time consuming
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    return nums[len(nums)-k]
+    
+# O(nk) time, selection sort idea
+def findKthLargest3(self, nums, k):
+    for i in xrange(len(nums), len(nums)-k, -1):
+        tmp = 0
+        for j in xrange(i):
+            if nums[j] > nums[tmp]:
+                tmp = j
+        nums[tmp], nums[i-1] = nums[i-1], nums[tmp]
+    return nums[len(nums)-k]
+    
+# O(k+(n-k)lgk) time, min-heap
+def findKthLargest4(self, nums, k):
+    heap = []
+    for num in nums:
+        heapq.heappush(heap, num)
+    for _ in xrange(len(nums)-k):
+        heapq.heappop(heap)
+    return heapq.heappop(heap)
+    
+def findKthLargest(self, nums, k):
+    heap = nums[:k]
+    heapify(heap)
+    for n in nums[k:]:
+        heappushpop(heap, n)
+    return heap[0]
 
-class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for (int num : nums) {
-            pq.push(num);
-            if (pq.size() > k) {
-                pq.pop();
-            }
-        }
-        return pq.top();
-    }
-};
+# O(k+(n-k)lgk) time, min-heap        
+def findKthLargest5(self, nums, k):
+    return heapq.nlargest(k, nums)[-1]
+    
+# O(n) time, quick selection
+def findKthLargest(self, nums, k):
+    # convert the kth largest to smallest
+    return self.findKthSmallest(nums, len(nums)+1-k)
+    
+def findKthSmallest(self, nums, k):
+    if nums:
+        pos = self.partition(nums, 0, len(nums)-1)
+        if k > pos+1:
+            return self.findKthSmallest(nums[pos+1:], k-pos-1)
+        elif k < pos+1:
+            return self.findKthSmallest(nums[:pos], k)
+        else:
+            return nums[pos]
+ 
+# choose the right-most element as pivot   
+def partition(self, nums, l, r):
+    low = l
+    while l < r:
+        if nums[l] < nums[r]:
+            nums[l], nums[low] = nums[low], nums[l]
+            low += 1
+        l += 1
+    nums[low], nums[r] = nums[r], nums[low]
+    return low
 
   
   
