f7bbed9ded962279baec9c18cbe2d3ca783e2f74
Q2016 <k.sohraby@gmail.com>
2022-06-21 15:37:11 -0700

    Update 202. Happy Number.py

--- 202. Happy Number.py
+++ 202. Happy Number.py
@@ -32,7 +32,7 @@ class Solution:
         return True if n==1 else False
     
 There are 2 parts to the algorithm we'll need to design and code.
-1)Given a number nn, what is its next number?
+1)Given a number n, what is its next number?
 2)Follow a chain of numbers and detect if we've entered a cycle.
 Part 1: can be done by using the division and modulus operators to repeatedly take digits off the number until none remain, 
 and then squaring each removed digit and adding them together. 
@@ -61,3 +61,27 @@ def isHappy(self, n: int) -> bool:
         n = get_next(n)
 
     return n == 1    
+
+
+
+Similar question:
+    
+The Collatz Conjecture says if you take a positive integer N and repeatedly set either N=N/2 (if it's even) or N=3N+1 (if it's odd), 
+N will eventually be 1.
+5 -> 16 -> 8 -> 4 -> 2 -> 1 (5 steps).
+Given N, how many steps does it take to reach 1?    
+
+
+Recursive solution with memoization
+
+def  collatz(num, memo={1: 0}):
+     if num in memo:
+         return memo[num]
+
+    if num % 2:
+        res = 1 + collatz(3*num + 1, memo)
+    else:
+        res = 1 + collatz(num//2, memo)
+
+    memo[num] = res
+    return res
