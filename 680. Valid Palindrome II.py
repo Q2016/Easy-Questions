9eda8c97c8873e397836657a7cff2167b95a23ba
Q2016 <k.sohraby@gmail.com>
2022-06-20 21:31:40 -0700

    Update 680. Valid Palindrome II.py

--- 680. Valid Palindrome II.py
+++ 680. Valid Palindrome II.py
@@ -13,6 +13,15 @@ Output: true
     
 Solution: Recursive
 For figure: https://leetcode.com/problems/valid-palindrome-ii/discuss/1452155/Python-Visual-Explanation-Picture-is-worth-a-thousand-words    
+
+1- s is a palindrome - great, we can just return true.
+
+2- Somewhere in s, there will be a pair of mismatched characters. We must use our allowed deletion on one of these characters. 
+Try both options - if neither results in a palindrome, then return false. Otherwise, return true. We can "delete" the character at j 
+by moving our bounds to (i, j - 1). Likewise, we can "delete" the character at i by moving our bounds to (i + 1, j).
+
+    
+    
     
     def validPalindrome(self, s):
         i, j = 0, len(s) - 1
@@ -34,3 +43,23 @@ For figure: https://leetcode.com/problems/valid-palindrome-ii/discuss/1452155/Py
                 return False
         
         return True     
+      
+      
+Complexity Analysis
+
+Given N as the length of s,
+
+Time complexity: O(N).
+
+The main while loop we use can iterate up to N / 2 times, since each iteration represents a pair of characters. On any given iteration, 
+we may find a mismatch and call checkPalindrome twice. checkPalindrome can also iterate up to N / 2 times, in the worst case where the 
+first and last character of s do not match.
+
+Because we are only allowed up to one deletion, the algorithm only considers one mismatch. This means that checkPalindrome will never be 
+called more than twice.
+
+As such, we have a time complexity of O(N).
+
+Space complexity: O(1).
+
+The only extra space used is by the two pointers i and j, which can be considered constant relative to the input size.
